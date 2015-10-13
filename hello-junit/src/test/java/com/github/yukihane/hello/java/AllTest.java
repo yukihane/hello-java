d2c4bed6c360a7ca35b53fe081102294b8128071
yukihane <yukihane.feather@gmail.com>
2015-10-13 15:29:50 +0900

    TestSuiteを用いてテスト実行

---
+++ hello-junit/src/test/java/com/github/yukihane/hello/java/AllTest.java
@@ -0,0 +1,9 @@
+package com.github.yukihane.hello.java;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({ AppTest.class, App2Test.class })
+public class AllTest {
+}
