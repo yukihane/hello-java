07f691975787803ce5d483d4d89393e86910425f
yukihane <yukihane.feather@gmail.com>
2019-02-25 17:37:20 +0900

    swagger

--- bean-validation-rest/src/main/java/com/github/yukihane/java/beanvalidationrest/BeanValidationRestApplication.java
+++ bean-validation-rest/src/main/java/com/github/yukihane/java/beanvalidationrest/BeanValidationRestApplication.java
@@ -2,12 +2,28 @@ package com.github.yukihane.java.beanvalidationrest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class BeanValidationRestApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeanValidationRestApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(BeanValidationRestApplication.class, args);
+    }
+
+    @Bean
+    public Docket document() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        final ApiInfo apiInfo = new ApiInfo("Sample API", "", "terms of service", "", "", "", "");
+        return apiInfo;
+    }
 
 }
