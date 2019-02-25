18626c4aa36e3b61beee78ffe8bd5cff1235f0fa
yukihane <yukihane.feather@gmail.com>
2019-02-25 10:18:31 +0900

    lombokで書き直し

--- bean-validation/src/main/java/org/hibernate/validator/referenceguide/chapter01/Car.java
+++ bean-validation/src/main/java/org/hibernate/validator/referenceguide/chapter01/Car.java
@@ -3,24 +3,20 @@ package org.hibernate.validator.referenceguide.chapter01;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 
+@Data
+@AllArgsConstructor
 public class Car {
 
     @NotNull
-    private final String manufacturer;
+    private String manufacturer;
 
     @NotNull
     @Size(min = 2, max = 14)
-    private final String licensePlate;
+    private String licensePlate;
 
     @Min(2)
-    private final int seatCount;
-
-    public Car(final String manufacturer, final String licencePlate, final int seatCount) {
-        this.manufacturer = manufacturer;
-        this.licensePlate = licencePlate;
-        this.seatCount = seatCount;
-    }
-
-    // getters and setters ...
+    private int seatCount;
 }
