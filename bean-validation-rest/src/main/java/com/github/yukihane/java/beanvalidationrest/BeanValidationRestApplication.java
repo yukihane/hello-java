bece9f6e9a9b86271815487b6c5fef2ce5f997e1
yukihane <yukihane.feather@gmail.com>
2019-02-25 14:32:12 +0900

    Bean ValidationとRestを組み合わせるテストのためのプロジェクト

---
+++ bean-validation-rest/src/main/java/com/github/yukihane/java/beanvalidationrest/BeanValidationRestApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.java.beanvalidationrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanValidationRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanValidationRestApplication.class, args);
+	}
+
+}
