4beb115302cb02f1863861652ab31fe4b911692a
yukihane <yukihane.feather@gmail.com>
2019-03-05 10:02:38 +0900

    auto-generated files

---
+++ hibernate-issues/qiita20180224/src/main/java/com/github/yukihane/hibernateissues/qiita20180224/Qiita20180224Application.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.hibernateissues.qiita20180224;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qiita20180224Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qiita20180224Application.class, args);
+	}
+
+}
---
+++ hibernate-issues/qiita20180224/src/test/java/com/github/yukihane/hibernateissues/qiita20180224/Qiita20180224ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.yukihane.hibernateissues.qiita20180224;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Qiita20180224ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
