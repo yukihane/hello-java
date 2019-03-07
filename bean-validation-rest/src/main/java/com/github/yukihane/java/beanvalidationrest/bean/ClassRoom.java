1a91de4a4a1f2931214735346dd9daecff6a3c2e
yukihane <yukihane.feather@gmail.com>
2019-03-07 09:37:28 +0900

    整形

--- bean-validation-rest/src/main/java/com/github/yukihane/java/beanvalidationrest/bean/ClassRoom.java
+++ bean-validation-rest/src/main/java/com/github/yukihane/java/beanvalidationrest/bean/ClassRoom.java
@@ -5,7 +5,6 @@ import java.util.List;
 import javax.validation.Valid;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
