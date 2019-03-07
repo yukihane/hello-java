5df45903c08bc4d72404a3665cf31bc891ee4e83
yukihane <yukihane.feather@gmail.com>
2019-03-07 18:10:38 +0900

    graphql実行用初期テンプレート

---
+++ graphql/hello-graphql/src/main/java/com/github/yukihane/springboot/hellographql/HelloGraphqlApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.springboot.hellographql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloGraphqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloGraphqlApplication.class, args);
+	}
+
+}
