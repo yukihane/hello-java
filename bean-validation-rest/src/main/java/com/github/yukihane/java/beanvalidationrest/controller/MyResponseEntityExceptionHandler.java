19f5392406cfd52bbc0cefe5d53fdce94b0fc819
yukihane <yukihane.feather@gmail.com>
2019-03-07 11:20:00 +0900

    仕様確認用のログ出力

--- bean-validation-rest/src/main/java/com/github/yukihane/java/beanvalidationrest/controller/MyResponseEntityExceptionHandler.java
+++ bean-validation-rest/src/main/java/com/github/yukihane/java/beanvalidationrest/controller/MyResponseEntityExceptionHandler.java
@@ -2,16 +2,15 @@ package com.github.yukihane.java.beanvalidationrest.controller;
 
 import java.util.List;
 import java.util.stream.Collectors;
-import javax.validation.ConstraintViolationException;
+import java.util.stream.Stream;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.core.MethodParameter;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
@@ -24,13 +23,34 @@ public class MyResponseEntityExceptionHandler extends ResponseEntityExceptionHan
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(final MethodArgumentNotValidException ex,
             final HttpHeaders headers, final HttpStatus status, final WebRequest request) {
+
+        final MethodParameter param = ex.getParameter();
+        LOG.info("parameterName: {}", param.getParameterName());
+        LOG.info("parameterType: {}", param.getParameterType());
+
         final BindingResult br = ex.getBindingResult();
         LOG.info("Target: {}", br.getTarget());
-        LOG.info("AllErrors: {}", br.getAllErrors());
         LOG.info("ErrorCount: {}", br.getErrorCount());
-        LOG.info("FieldErrors: {}", br.getFieldErrors());
-        LOG.info("message: {}",
-                br.getAllErrors().stream().map(e -> e.getDefaultMessage()).collect(Collectors.toList()));
+
+        LOG.info("AllErrors");
+        br.getAllErrors().stream().forEach(e -> {
+            LOG.info("objectName: {}", e.getObjectName());
+            Stream.of(e.getCodes()).forEach(c -> {
+                LOG.info("code: {}", c);
+            });
+            Stream.of(e.getArguments()).forEach(a -> {
+                LOG.info("argument: {}", a);
+            });
+        });
+
+        LOG.info("fieldErrors");
+        br.getFieldErrors().stream().forEach(e -> {
+            LOG.info("objectName: {}", e.getObjectName());
+            LOG.info("field: {}", e.getField());
+            Stream.of(e.getArguments()).forEach(a -> {
+                LOG.info("argument: {}", a);
+            });
+        });
 
         final List<ConstraintViolationResponse> errors = br.getFieldErrors().stream()
                 .map(e -> new ConstraintViolationResponse(e.getField(), e.getCodes(), e.getObjectName(), e.getDefaultMessage()))
