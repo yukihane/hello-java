519a340fef241e7eeed28237c5e8e4a26e60eb82
yukihane <yukihane.feather@gmail.com>
2019-03-13 18:50:14 +0900

    auto-generated files

---
+++ spring/spring-boot-annotation-examples/src/main/java/com/example/springboot/springbootannotationexamples/SpringBootAnnotationExamplesApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.springbootannotationexamples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAnnotationExamplesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAnnotationExamplesApplication.class, args);
+	}
+
+}
