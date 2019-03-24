7bbede0f000e5b89dafc89034fbebef57ebe29ae
yukihane <yukihane.feather@gmail.com>
2019-03-25 03:28:28 +0900

    springfox組み込み

--- spring/spring-boot-annotation-examples/src/main/java/com/example/springboot/SpringBootAnnotationExamplesApplication.java
+++ spring/spring-boot-annotation-examples/src/main/java/com/example/springboot/SpringBootAnnotationExamplesApplication.java
@@ -2,7 +2,12 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * {@link SpringBootApplication} を付与したクラス(便宜上メインクラスと呼びます)を
@@ -13,10 +18,20 @@ import org.springframework.context.annotation.ComponentScan;
  * 通常メインクラスはパッケージ階層のトップレベルに置きます。
  */
 @SpringBootApplication
+@EnableSwagger2
 public class SpringBootAnnotationExamplesApplication {
 
     public static void main(final String[] args) {
         SpringApplication.run(SpringBootAnnotationExamplesApplication.class, args);
     }
 
+    @Bean
+    public Docket document() {
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        final ApiInfo apiInfo = new ApiInfo("Sample API", "", "terms of service", "", "", "", "");
+        return apiInfo;
+    }
 }
