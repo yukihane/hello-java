2369a4cfb84e3bbe0157911981e3f16d1a39ff3d
yukihane <yukihane.feather@gmail.com>
2019-03-25 14:10:15 +0900

    auto-generated files

---
+++ mapstruct/hello-mapstruct/src/main/java/com/github/yukihane/hello_mapstruct/App.java
@@ -0,0 +1,11 @@
+package com.github.yukihane.hello_mapstruct;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ mapstruct/hello-mapstruct/src/test/java/com/github/yukihane/hello_mapstruct/AppTest.java
@@ -0,0 +1,19 @@
+package com.github.yukihane.hello_mapstruct;
+
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true);
+    }
+}
