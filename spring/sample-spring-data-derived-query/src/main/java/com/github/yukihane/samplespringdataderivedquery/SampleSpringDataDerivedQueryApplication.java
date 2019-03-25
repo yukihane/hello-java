bfe3644a8af656570ade1f154452609efe029039
yukihane <yukihane.feather@gmail.com>
2019-03-25 11:44:22 +0900

    initial files

---
+++ spring/sample-spring-data-derived-query/src/main/java/com/github/yukihane/samplespringdataderivedquery/SampleSpringDataDerivedQueryApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.samplespringdataderivedquery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleSpringDataDerivedQueryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleSpringDataDerivedQueryApplication.class, args);
+	}
+
+}
