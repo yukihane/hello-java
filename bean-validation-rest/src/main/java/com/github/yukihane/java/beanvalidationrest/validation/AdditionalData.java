e4cf97c7648aae8ad891ad339bea5f14cfd0a3f0
yukihane <yukihane.feather@gmail.com>
2019-03-29 10:14:58 +0900

    commit漏れ

---
+++ bean-validation-rest/src/main/java/com/github/yukihane/java/beanvalidationrest/validation/AdditionalData.java
@@ -0,0 +1,10 @@
+package com.github.yukihane.java.beanvalidationrest.validation;
+
+import java.util.List;
+import lombok.Data;
+
+@Data
+public class AdditionalData {
+
+    List<String> fields;
+}
