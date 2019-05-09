0b97f6cf2c3c8d778b4c8edf8b997c50723922e0
yukihane <yukihane.feather@gmail.com>
2019-05-10 04:37:45 +0900

    auto-generated files

---
+++ spring/spring-security-auth-example/src/main/java/com/github/yukihane/springsecurityauthexample/SpringSecurityAuthExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.springsecurityauthexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthExampleApplication.class, args);
+	}
+
+}
