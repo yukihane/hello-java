8c9d6579363e7f0bacd3879182715aa568e203ae
yukihane <yukihane.feather@gmail.com>
2019-06-01 08:14:29 +0900

    通常のinject

--- spring/aspectj/src/main/java/com/github/yukihane/spring/aspectj/AspectjApplication.java
+++ spring/aspectj/src/main/java/com/github/yukihane/spring/aspectj/AspectjApplication.java
@@ -1,11 +1,16 @@
 package com.github.yukihane.spring.aspectj;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class AspectjApplication implements CommandLineRunner {
+
+    @Autowired
+    private MyComponent myComponent;
 
     public static void main(final String[] args) {
         SpringApplication.run(AspectjApplication.class, args);
@@ -13,7 +18,13 @@ public class AspectjApplication implements CommandLineRunner {
 
     @Override
     public void run(final String... args) throws Exception {
-        System.out.println("Hello, world!");
+        System.out.println("Hello, " + myComponent.getName());
     }
 
+    @Component
+    public static class MyComponent {
+        public String getName() {
+            return this.getClass().getSimpleName();
+        }
+    }
 }
