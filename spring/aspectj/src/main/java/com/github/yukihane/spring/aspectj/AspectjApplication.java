e440e19c3e7c430f8f1c70dd2599ee47dbf69622
yukihane <yukihane.feather@gmail.com>
2019-06-01 08:06:37 +0900

    auto-generated files

---
+++ spring/aspectj/src/main/java/com/github/yukihane/spring/aspectj/AspectjApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.spring.aspectj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AspectjApplication.class, args);
+	}
+
+}
