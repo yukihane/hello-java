5147967a74a050c79778f7c57f7730f41de8f021
yukihane <yukihane.feather@gmail.com>
2019-06-01 08:10:42 +0900

    コマンドライン実行雛形

--- spring/aspectj/src/main/java/com/github/yukihane/spring/aspectj/AspectjApplication.java
+++ spring/aspectj/src/main/java/com/github/yukihane/spring/aspectj/AspectjApplication.java
@@ -1,13 +1,19 @@
 package com.github.yukihane.spring.aspectj;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AspectjApplication {
+public class AspectjApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AspectjApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(AspectjApplication.class, args);
+    }
+
+    @Override
+    public void run(final String... args) throws Exception {
+        System.out.println("Hello, world!");
+    }
 
 }
