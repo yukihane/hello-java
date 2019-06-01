18e7518fdea20daf4559edd12e3798d86e752052
yukihane <yukihane.feather@gmail.com>
2019-06-01 21:05:59 +0900

    時刻は固定にする

--- spring/mybatis-datetime/src/main/java/com/github/yukihane/spring/mybatisdatetime/ClockConfig.java
+++ spring/mybatis-datetime/src/main/java/com/github/yukihane/spring/mybatisdatetime/ClockConfig.java
@@ -1,13 +1,22 @@
 package com.github.yukihane.spring.mybatisdatetime;
 
 import java.time.Clock;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ClockConfig {
+
     @Bean
     public Clock clock() {
-        return Clock.systemDefaultZone();
+        final ZoneId tokyo = ZoneId.of("Asia/Tokyo");
+        final LocalDate date = LocalDate.of(2010, 10, 10);
+        final Instant fixedInstant = date.atStartOfDay(tokyo).toInstant();
+        final Clock clock = Clock.fixed(fixedInstant, tokyo);
+
+        return clock;
     }
 }
