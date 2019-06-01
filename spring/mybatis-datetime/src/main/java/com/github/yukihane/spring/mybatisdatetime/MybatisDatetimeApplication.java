0cfffbf89aafb3a676674d640bea0bc71372d243
yukihane <yukihane.feather@gmail.com>
2019-06-01 12:27:37 -0400

    Revert "非SpringコンポーネントでSpringコンポーネントを利用するための設定"
    This reverts commit 07b75223c883abf1007c1b0f51d76f35ff092dd3.

--- spring/mybatis-datetime/src/main/java/com/github/yukihane/spring/mybatisdatetime/MybatisDatetimeApplication.java
+++ spring/mybatis-datetime/src/main/java/com/github/yukihane/spring/mybatisdatetime/MybatisDatetimeApplication.java
@@ -2,9 +2,7 @@ package com.github.yukihane.spring.mybatisdatetime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
-@EnableSpringConfigured
 @SpringBootApplication
 public class MybatisDatetimeApplication {
 
