4baf2d0bbd07b6b0e376ef92825278846fecf131
yukihane <yukihane.feather@gmail.com>
2019-06-01 17:47:52 +0900

    今回使用するDBスキーマ

---
+++ spring/mybatis-datetime/src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into my_datetime(id, col_timestamp) values(1, '2019-06-01 10:15:40')
---
+++ spring/mybatis-datetime/src/main/resources/schema.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS my_datetime;
+CREATE TABLE my_datetime (
+    id integer primary key,
+    col_timestamp TIMESTAMP NULL
+);
