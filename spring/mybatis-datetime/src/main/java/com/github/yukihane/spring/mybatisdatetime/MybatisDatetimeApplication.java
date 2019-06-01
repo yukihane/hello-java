07b75223c883abf1007c1b0f51d76f35ff092dd3
yukihane <yukihane.feather@gmail.com>
2019-06-01 11:31:13 -0400

    非SpringコンポーネントでSpringコンポーネントを利用するための設定 https://qiita.com/yukihane/items/93613eb201d274645ac6

--- spring/mybatis-datetime/src/main/java/com/github/yukihane/spring/mybatisdatetime/MybatisDatetimeApplication.java
+++ spring/mybatis-datetime/src/main/java/com/github/yukihane/spring/mybatisdatetime/MybatisDatetimeApplication.java
@@ -2,7 +2,9 @@ package com.github.yukihane.spring.mybatisdatetime;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
 
+@EnableSpringConfigured
 @SpringBootApplication
 public class MybatisDatetimeApplication {
 
