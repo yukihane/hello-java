f4fc987621166a99721921b3758eb5c37936845e
yukihane <yukihane.feather@gmail.com>
2019-06-01 17:11:47 +0900

    auto-generated files

---
+++ spring/mybatis-datetime/src/main/java/com/github/yukihane/spring/mybatisdatetime/MybatisDatetimeApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.spring.mybatisdatetime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisDatetimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisDatetimeApplication.class, args);
+	}
+
+}
