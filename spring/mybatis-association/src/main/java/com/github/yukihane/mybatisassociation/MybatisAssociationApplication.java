486412815277358f7ce5f87422e0d623f067c23f
yukihane <yukihane.feather@gmail.com>
2019-06-22 07:37:25 +0900

    auto-generated files

---
+++ spring/mybatis-association/src/main/java/com/github/yukihane/mybatisassociation/MybatisAssociationApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.mybatisassociation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisAssociationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisAssociationApplication.class, args);
+	}
+
+}
