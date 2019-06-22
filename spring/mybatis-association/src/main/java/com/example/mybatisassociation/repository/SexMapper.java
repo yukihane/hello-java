58ea098cd00e54c0b4dd7369c65ad1620c922200
yukihane <yukihane.feather@gmail.com>
2019-06-22 22:28:27 +0900

    sex sqlをxmlで定義

--- spring/mybatis-association/src/main/java/com/example/mybatisassociation/repository/SexMapper.java
+++ spring/mybatis-association/src/main/java/com/example/mybatisassociation/repository/SexMapper.java
@@ -4,14 +4,11 @@ import com.example.mybatisassociation.entity.Sex;
 import java.util.List;
 import java.util.Optional;
 import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface SexMapper {
 
-    @Select("select * from sex where id = #{id}")
     Optional<Sex> findById(long id);
 
-    @Select("select * from sex")
     List<Sex> findAll();
 }
