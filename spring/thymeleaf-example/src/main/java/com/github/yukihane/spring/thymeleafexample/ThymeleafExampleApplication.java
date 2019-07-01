e29750526c7c41673774551fdf1fa17bbc55b375
yukihane <yukihane.feather@gmail.com>
2019-07-02 01:03:48 +0900

    auto-generated files

---
+++ spring/thymeleaf-example/src/main/java/com/github/yukihane/spring/thymeleafexample/ThymeleafExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.spring.thymeleafexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafExampleApplication.class, args);
+	}
+
+}
---
+++ spring/thymeleaf-example/src/test/java/com/github/yukihane/spring/thymeleafexample/ThymeleafExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.yukihane.spring.thymeleafexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ThymeleafExampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
