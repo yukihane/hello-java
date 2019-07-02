332569e2d547824ec44b142d02bb7c594cc5569e
yukihane <yukihane.feather@gmail.com>
2019-07-02 11:10:30 +0900

    整形

--- spring/oauth2-example/src/main/java/com/example/oauth2example/Oauth2ExampleApplication.java
+++ spring/oauth2-example/src/main/java/com/example/oauth2example/Oauth2ExampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Oauth2ExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Oauth2ExampleApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(Oauth2ExampleApplication.class, args);
+    }
 
 }
--- spring/oauth2-example/src/test/java/com/example/oauth2example/Oauth2ExampleApplicationTests.java
+++ spring/oauth2-example/src/test/java/com/example/oauth2example/Oauth2ExampleApplicationTests.java
@@ -9,8 +9,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class Oauth2ExampleApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
