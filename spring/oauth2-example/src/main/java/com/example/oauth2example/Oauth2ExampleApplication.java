aed31c71d938797d567723d6628c78e4ea75b782
yukihane <yukihane.feather@gmail.com>
2019-07-02 11:11:25 +0900

    add @EnableAuthorizationServer

--- spring/oauth2-example/src/main/java/com/example/oauth2example/Oauth2ExampleApplication.java
+++ spring/oauth2-example/src/main/java/com/example/oauth2example/Oauth2ExampleApplication.java
@@ -2,7 +2,9 @@ package com.example.oauth2example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 
+@EnableAuthorizationServer
 @SpringBootApplication
 public class Oauth2ExampleApplication {
 
