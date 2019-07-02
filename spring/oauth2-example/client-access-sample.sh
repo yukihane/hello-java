f60ae668e843113683be1d5e5f71945f0f67aba7
yukihane <yukihane.feather@gmail.com>
2019-07-02 11:46:28 +0900

    プロパティ設定に合わせてスクリプト修正

--- spring/oauth2-example/client-access-sample.sh
+++ spring/oauth2-example/client-access-sample.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-# それぞれ起動時ログに出力されている次の値
+# spring.security.user.name プロパティ
+# 未設定なら user
+username=resource-owner-id
+
+# spring.security.user.password プロパティ.
+# 未設定なら起動時ログに出力される次の値
 # Using generated security password: 5bd786c2-7cbb-4c32-9fc9-778de3f3d94b
-pass=5bd786c2-7cbb-4c32-9fc9-778de3f3d94b
+pass=resource-owner-pass
 
+# security.oauth2.client.client-id プロパティ
+# 未設定なら起動時ログに出力される次の値
 # security.oauth2.client.client-id = 38b63a6f-0afb-4ede-8ac5-48cddaa72cd4
-clientid=38b63a6f-0afb-4ede-8ac5-48cddaa72cd4
+clientid=clid
 
+# security.oauth2.client.client-secret プロパティ
+# 未設定なら起動時ログに出力される次の値
 # security.oauth2.client.client-secret = a427c2df-b0ff-4bc1-af5d-70b9962bc79b
-clientsec=a427c2df-b0ff-4bc1-af5d-70b9962bc79b
+clientsec=clsec
 
-curl $clientid:$clientsec@localhost:8080/oauth/token -d grant_type=password -d username=user -d password=$pass -d scope=any
+curl $clientid:$clientsec@localhost:8080/oauth/token -d grant_type=password -d username=$username -d password=$pass -d scope=any
