bf64f792625a99cb803f72d56cc5e577a0364593
yukihane <yukihane.feather@gmail.com>
2019-07-02 23:08:10 +0900

    コピペった不要ログ出力を除去

--- spring/spring-security-auth-example/src/main/java/com/github/yukihane/springsecurityauthexample/config/MyWebSecurityConfigurerAdapter.java
+++ spring/spring-security-auth-example/src/main/java/com/github/yukihane/springsecurityauthexample/config/MyWebSecurityConfigurerAdapter.java
@@ -2,12 +2,10 @@ package com.github.yukihane.springsecurityauthexample.config;
 
 import com.github.yukihane.springsecurityauthexample.security.MyAuthenticationProvider;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-@Slf4j
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter {
@@ -16,9 +14,6 @@ public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        log.debug(
-            "Using default configure(HttpSecurity). If subclassed this will potentially override subclass configure(HttpSecurity).");
-
         http
             .authorizeRequests()
             .anyRequest().authenticated()
