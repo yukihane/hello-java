f8637f15555fc566922938455610856ddd3b1dd6
yukihane <yukihane.feather@gmail.com>
2019-07-02 10:16:10 +0900

    auto-generated files

---
+++ spring/oauth2-example/src/main/java/com/example/oauth2example/Oauth2ExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ExampleApplication.class, args);
+	}
+
+}
