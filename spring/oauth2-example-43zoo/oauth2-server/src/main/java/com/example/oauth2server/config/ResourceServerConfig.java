ae74d2aa01035eac1ba6f4e69e59b42b2dac237f
yukihane <yukihane.feather@gmail.com>
2019-07-03 20:40:46 +0900

    リソースサーバ認可設定

---
+++ spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/config/ResourceServerConfig.java
@@ -0,0 +1,19 @@
+package com.example.oauth2server.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration
+public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .antMatchers("/api/**").access("#oauth2.hasScope('read')");
+
+        // ↓を行うとログイン画面に行くのに認証が必要になってしまう
+        //            .anyRequest().authenticated();
+    }
+}
