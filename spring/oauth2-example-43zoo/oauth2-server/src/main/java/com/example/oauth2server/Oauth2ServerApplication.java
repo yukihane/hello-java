af21b94be711a9cb36747afe4e07961194558d89
yukihane <yukihane.feather@gmail.com>
2019-07-03 18:43:42 +0900

    @EnableResourceServer 付与

--- spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/Oauth2ServerApplication.java
+++ spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/Oauth2ServerApplication.java
@@ -3,10 +3,12 @@ package com.example.oauth2server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 // 11.3. Expression-Based Access Control
 // https://docs.spring.io/spring-security/site/docs/5.1.5.RELEASE/reference/htmlsingle/#el-pre-post-annotations
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableResourceServer
 @SpringBootApplication
 public class Oauth2ServerApplication {
 
