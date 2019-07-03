dce1c64965c7c5ae22f6167825105cdf89d8af45
yukihane <yukihane.feather@gmail.com>
2019-07-03 20:15:59 +0900

    認可サーバ仮設定

--- spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/Oauth2ServerApplication.java
+++ spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/Oauth2ServerApplication.java
@@ -3,11 +3,13 @@ package com.example.oauth2server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 // 11.3. Expression-Based Access Control
 // https://docs.spring.io/spring-security/site/docs/5.1.5.RELEASE/reference/htmlsingle/#el-pre-post-annotations
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableAuthorizationServer
 @EnableResourceServer
 @SpringBootApplication
 public class Oauth2ServerApplication {
---
+++ spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/config/AuthorizationServerConfig.java
@@ -0,0 +1,30 @@
+package com.example.oauth2server.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+
+// https://docs.spring.io/spring-security-oauth2-boot/docs/current-SNAPSHOT/reference/html5/#registering-a-redirect-uri-with-the-client
+@Configuration
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Override
+    public void configure(final ClientDetailsServiceConfigurer clients) throws Exception {
+        final String secret = passwordEncoder().encode("noonewilleverguess");
+        clients
+            .inMemory()
+            .withClient("first-client")
+            .secret(secret)
+            .scopes("read")
+            .authorizedGrantTypes("authorization_code")
+            .redirectUris("http://localhost:8081/oauth/login/client-app");
+    }
+}
