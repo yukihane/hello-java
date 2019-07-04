b0f6222cecc89052a01a8aa6e2f322eb155e04d9
yukihane <yukihane.feather@gmail.com>
2019-07-04 14:06:14 +0900

    自動設定の認証をOFFにする

--- spring/oauth2-example-43zoo/oauth2-client/src/main/java/com/example/oauth2client/Oauth2ClientApplication.java
+++ spring/oauth2-example-43zoo/oauth2-client/src/main/java/com/example/oauth2client/Oauth2ClientApplication.java
@@ -2,8 +2,9 @@ package com.example.oauth2client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class Oauth2ClientApplication {
 
     public static void main(final String[] args) {
