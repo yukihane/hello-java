f0336ba23228b02d92cdb7654b350595e6da511b
yukihane <yukihane.feather@gmail.com>
2019-07-09 12:38:07 +0900

    Basic認証用フィルタ

---
+++ spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/security/MyBasicAuthFilter.java
@@ -0,0 +1,17 @@
+package com.example.oauth2server.security;
+
+import java.util.Arrays;
+
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyBasicAuthFilter extends BasicAuthenticationFilter {
+
+
+    public MyBasicAuthFilter(final MyAuthenticationProvider provider) {
+        super(new ProviderManager(Arrays.asList(provider)));
+    }
+
+}
