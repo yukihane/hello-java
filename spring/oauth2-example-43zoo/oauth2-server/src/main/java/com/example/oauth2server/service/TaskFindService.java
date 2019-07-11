f0cb6aac0aa632dcce1a7d9a9d90705fa67b14b8
yukihane <yukihane.feather@gmail.com>
2019-07-11 17:57:32 +0900

    ac13302f39cba02 で実装したアクセスコントロール処理を一旦削除

--- spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/service/TaskFindService.java
+++ spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/service/TaskFindService.java
@@ -2,17 +2,12 @@ package com.example.oauth2server.service;
 
 import java.util.List;
 
-import org.springframework.security.access.prepost.PostAuthorize;
-import org.springframework.security.access.prepost.PostFilter;
-
 import com.example.oauth2server.entity.Task;
 
 public interface TaskFindService {
 
-    @PostFilter("filterObject.creator.name == authentication.name")
     List<Task> findAll();
 
-    @PostAuthorize("returnObject.creator.name == authentication.name")
     Task findById(Long id);
 
 }
