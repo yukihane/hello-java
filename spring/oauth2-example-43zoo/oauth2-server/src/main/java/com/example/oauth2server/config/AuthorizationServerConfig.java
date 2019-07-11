10dcd08c7d14b03cd261bc08cb974f5a5774270f
yukihane <yukihane.feather@gmail.com>
2019-07-11 18:57:35 +0900

    https://stackoverflow.com/a/45698964/4506703 を参考に実装(動作しない)

--- spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/config/AuthorizationServerConfig.java
+++ spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/config/AuthorizationServerConfig.java
@@ -1,5 +1,7 @@
 package com.example.oauth2server.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -7,11 +9,30 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+
+import lombok.RequiredArgsConstructor;
 
 // https://docs.spring.io/spring-security-oauth2-boot/docs/current-SNAPSHOT/reference/html5/#registering-a-redirect-uri-with-the-client
 @EnableAuthorizationServer
 @Configuration
+@RequiredArgsConstructor
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    private final DataSource dataSource;
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JdbcTokenStore(dataSource);
+    }
+
+    @Bean
+    public JdbcAuthorizationCodeServices jdbcAuthorizationCodeServices() {
+        return new JdbcAuthorizationCodeServices(dataSource);
+    }
 
     @Bean
     PasswordEncoder passwordEncoder() {
@@ -28,5 +49,13 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
             .scopes("read")
             .authorizedGrantTypes("authorization_code")
             .redirectUris("http://localhost:8081/client/tasks");
+    }
+
+    @Override
+    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints
+            .tokenStore(tokenStore())
+            .reuseRefreshTokens(false)
+            .authorizationCodeServices(jdbcAuthorizationCodeServices());
     }
 }
