e46f8d692affb29bf87fe4661b9d8ee7542d26f0
yukihane <yukihane.feather@gmail.com>
2019-07-12 14:47:16 +0900

    refresh_token用テーブル

--- spring/oauth2-example-43zoo/oauth2-server/src/main/resources/schema.sql
+++ spring/oauth2-example-43zoo/oauth2-server/src/main/resources/schema.sql
@@ -1,3 +1,8 @@
+/*
+https://github.com/spring-projects/spring-security-oauth/blob/master/spring-security-oauth2/src/test/resources/schema.sql
+から必要なものをピックアップ
+*/
+
 create table if not exists oauth_code (
   code VARCHAR(256),
   authentication LONGVARBINARY
@@ -11,4 +16,10 @@ create table if not exists oauth_access_token (
   client_id VARCHAR(256),
   authentication LONGVARBINARY,
   refresh_token VARCHAR(256)
+);
+
+create table if not exists oauth_refresh_token (
+  token_id VARCHAR(256),
+  token LONGVARBINARY,
+  authentication LONGVARBINARY
 );
