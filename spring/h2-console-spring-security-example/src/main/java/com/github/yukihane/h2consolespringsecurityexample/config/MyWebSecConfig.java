15e7b26b3b23ec2c3df4218ff20bbc2b58aed9ed
yukihane <yukihane.feather@gmail.com>
2019-07-12 12:00:16 +0900

    webセキュリティコンフィグで要認証を無効化

---
+++ spring/h2-console-spring-security-example/src/main/java/com/github/yukihane/h2consolespringsecurityexample/config/MyWebSecConfig.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.h2consolespringsecurityexample.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class MyWebSecConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+    }
+}
