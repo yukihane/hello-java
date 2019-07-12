8113d7bc552c11f3e1cf3d6e64fe97be922d2596
yukihane <yukihane.feather@gmail.com>
2019-07-12 10:56:55 +0900

    auto-generated files

---
+++ spring/h2-console-spring-security-example/src/main/java/com/github/yukihane/h2consolespringsecurityexample/H2ConsoleSpringSecurityExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.h2consolespringsecurityexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2ConsoleSpringSecurityExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2ConsoleSpringSecurityExampleApplication.class, args);
+	}
+
+}
