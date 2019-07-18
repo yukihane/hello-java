74c1e02ea7dcc2ad7923ab209a350584b4d94964
yukihane <yukihane.feather@gmail.com>
2019-07-18 09:36:09 +0900

    デフォルトWebSecurityConfig

---
+++ spring/filter-auto-registration-example/src/main/java/com/example/filterautoregistrationexample/WebSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.example.filterautoregistrationexample;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+    }
+}
