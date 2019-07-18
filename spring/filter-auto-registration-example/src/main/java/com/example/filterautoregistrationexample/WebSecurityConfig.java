6c9ed4694d4e2a2cc83f4eb50b4db6ef04fa5a0e
yukihane <yukihane.feather@gmail.com>
2019-07-18 11:08:05 +0900

    認証フィルタ適用

--- spring/filter-auto-registration-example/src/main/java/com/example/filterautoregistrationexample/WebSecurityConfig.java
+++ spring/filter-auto-registration-example/src/main/java/com/example/filterautoregistrationexample/WebSecurityConfig.java
@@ -5,13 +5,22 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import com.example.filterautoregistrationexample.auth.MyHeaderAuthenticationFilter;
+
+import lombok.RequiredArgsConstructor;
+
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final MyHeaderAuthenticationFilter filter;
+
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.antMatcher("/api/private/**")
             .authorizeRequests().anyRequest()
             .authenticated();
+        http.addFilter(filter);
     }
 }
