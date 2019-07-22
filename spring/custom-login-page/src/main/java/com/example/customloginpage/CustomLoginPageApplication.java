a675dbf5506f6e52909bdb545f2a8d1a1e7d0cc9
yukihane <yukihane.feather@gmail.com>
2019-07-22 18:39:35 +0900

    auto-generated files

---
+++ spring/custom-login-page/src/main/java/com/example/customloginpage/CustomLoginPageApplication.java
@@ -0,0 +1,13 @@
+package com.example.customloginpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomLoginPageApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(CustomLoginPageApplication.class, args);
+    }
+
+}
