f8b412481e6f791b5ed3fd4624ebbfe03f2fe94e
yukihane <yukihane.feather@gmail.com>
2019-07-23 10:00:49 +0900

    コントローラ調整

--- spring/custom-login-page/src/main/java/com/example/customloginpage/controller/HelloController.java
+++ spring/custom-login-page/src/main/java/com/example/customloginpage/controller/HelloController.java
@@ -1,13 +1,21 @@
 package com.example.customloginpage.controller;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
 
-    @GetMapping("/hello")
+    @GetMapping("/world")
     public String hello() {
-        return "hello";
+        return "Hello, world!";
+    }
+
+    @GetMapping("/user")
+    public String user(final Authentication auth) {
+        return "Hello, " + auth.getName() + "!";
     }
 }
