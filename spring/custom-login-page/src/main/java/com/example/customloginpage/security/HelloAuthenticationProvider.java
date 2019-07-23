a76a7adc495b42b6a2b1b8ca8bd323484c45abf6
yukihane <yukihane.feather@gmail.com>
2019-07-23 13:20:07 +0900

    auth provider

---
+++ spring/custom-login-page/src/main/java/com/example/customloginpage/security/HelloAuthenticationProvider.java
@@ -0,0 +1,22 @@
+package com.example.customloginpage.security;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+
+public class HelloAuthenticationProvider implements AuthenticationProvider {
+
+    @Override
+    public Authentication authenticate(final Authentication authentication) throws AuthenticationException {
+        final UsernamePasswordAuthenticationToken auth = (UsernamePasswordAuthenticationToken) authentication;
+        auth.setAuthenticated(true);
+        return auth;
+    }
+
+    @Override
+    public boolean supports(final Class<?> authentication) {
+        return UsernamePasswordAuthenticationToken.class.equals(authentication);
+    }
+
+}
