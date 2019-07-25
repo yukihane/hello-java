657f33527c2334c75349dff1406ad9760aebbdef
yukihane <yukihane.feather@gmail.com>
2019-07-25 18:00:59 +0900

    refresh_tokenを確認したいのでクライアントでもユーザ認証を行う(ことでセッションを維持する)

--- spring/oauth2-example-43zoo/oauth2-client/src/main/java/com/example/oauth2client/Oauth2ClientApplication.java
+++ spring/oauth2-example-43zoo/oauth2-client/src/main/java/com/example/oauth2client/Oauth2ClientApplication.java
@@ -2,9 +2,8 @@ package com.example.oauth2client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class Oauth2ClientApplication {
 
     public static void main(final String[] args) {
