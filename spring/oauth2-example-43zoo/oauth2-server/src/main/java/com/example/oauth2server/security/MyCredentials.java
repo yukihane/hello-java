d56c227e8f3a21146294c2aa0d8e09e0b5b6bda5
yukihane <yukihane.feather@gmail.com>
2019-07-30 14:17:00 +0900

    Authenticationを永続化しているようで、その要素のprincipal, credentialsがSerializableでなければならないらしい

--- spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/security/MyCredentials.java
+++ spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/security/MyCredentials.java
@@ -1,8 +1,12 @@
 package com.example.oauth2server.security;
 
+import java.io.Serializable;
 import lombok.Data;
 
 @Data
-public class MyCredentials {
+public class MyCredentials implements Serializable {
+
+    private static final long serialVersionUID = -4658330939385972601L;
+
     private final String name;
 }
--- spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/security/MyPrincipal.java
+++ spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/security/MyPrincipal.java
@@ -1,10 +1,13 @@
 package com.example.oauth2server.security;
 
+import java.io.Serializable;
 import lombok.Data;
 import org.springframework.security.core.AuthenticatedPrincipal;
 
 @Data
-public class MyPrincipal implements AuthenticatedPrincipal {
+public class MyPrincipal implements AuthenticatedPrincipal, Serializable {
+
+    private static final long serialVersionUID = -4162556580473630722L;
 
     private final String name;
 
