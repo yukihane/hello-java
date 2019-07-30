2e7c01e75ae7e1ab92af2cccad6d5152d07147e4
yukihane <yukihane.feather@gmail.com>
2019-07-30 09:56:43 +0900

    OAuth系(認可サーバ)エンドポイントセキュリティコンフィグ

---
+++ spring/oauth2-example-43zoo/oauth2-server/src/main/java/com/example/oauth2server/config/OAuthEndPointSecurityConfig.java
@@ -0,0 +1,50 @@
+package com.example.oauth2server.config;
+
+import com.example.oauth2server.repository.UserRepository;
+import com.example.oauth2server.security.MyHeaderAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class OAuthEndPointSecurityConfig {
+
+    @Configuration
+    @Order(1)
+    @RequiredArgsConstructor
+    public static class HeaderAuthConfig extends WebSecurityConfigurerAdapter {
+
+        private final UserRepository userRepository;
+
+        @Override
+        protected void configure(final HttpSecurity http) throws Exception {
+            http.antMatcher("/v1/authenticate/signin")
+                .authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .addFilter(new MyHeaderAuthFilter(userRepository));
+        }
+    }
+
+    @Configuration
+    @Order(2)
+    public static class FormAuthConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(final HttpSecurity http) throws Exception {
+            http.antMatcher("/v1/**")
+                .formLogin()
+                .loginPage("/v1/authenticate/identity")
+                .loginProcessingUrl("/v1/authenticate/confirm")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/v1/authenticate/identity").permitAll()
+                .antMatchers("/v1/oauth/error").permitAll()
+                .antMatchers("/v1/oauth/authorize").authenticated()
+                .anyRequest().denyAll();
+        }
+    }
+}
