ca9e239b492763f2a07475c8d35199fd6b4de45c
yukihane <yukihane.feather@gmail.com>
2019-08-07 00:42:36 +0900

    プール数以上にDBコネクションを確保するテスト

--- spring/spring-test-connection-size/src/test/java/com/github/yukihane/springtestconnectionsize/SpringTestConnectionSizeApplicationTests.java
+++ spring/spring-test-connection-size/src/test/java/com/github/yukihane/springtestconnectionsize/SpringTestConnectionSizeApplicationTests.java
@@ -1,16 +1,33 @@
 package com.github.yukihane.springtestconnectionsize;
 
+import java.sql.Connection;
+import java.util.ArrayList;
+import java.util.List;
+import javax.sql.DataSource;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.datasource.DataSourceUtils;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringTestConnectionSizeApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void contextLoads() {
+        final List<Connection> conns = new ArrayList<>();
+        // デフォルトのコネクションプール数は10なので失敗する
+        for (int i = 0; i < 20; i++) {
+            final Connection c = DataSourceUtils.getConnection(dataSource);
+            conns.add(c);
+            System.out.println(c);
+        }
+
+    }
 
 }
