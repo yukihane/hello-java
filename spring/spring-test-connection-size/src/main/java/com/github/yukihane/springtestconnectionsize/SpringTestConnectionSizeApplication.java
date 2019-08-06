e84fcf3c36219a4945b934b8202367c9ca9c533c
yukihane <yukihane.feather@gmail.com>
2019-08-07 00:30:43 +0900

    auto-generated files

---
+++ spring/spring-test-connection-size/src/main/java/com/github/yukihane/springtestconnectionsize/SpringTestConnectionSizeApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.springtestconnectionsize;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTestConnectionSizeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTestConnectionSizeApplication.class, args);
+	}
+
+}
---
+++ spring/spring-test-connection-size/src/test/java/com/github/yukihane/springtestconnectionsize/SpringTestConnectionSizeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.yukihane.springtestconnectionsize;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringTestConnectionSizeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
