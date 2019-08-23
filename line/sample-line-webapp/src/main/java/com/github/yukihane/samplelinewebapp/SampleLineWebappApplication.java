e9e09ba34e1fafa24117c79d6675a930942068ea
yukihane <yukihane.feather@gmail.com>
2019-08-23 10:13:47 +0900

    整形

--- line/sample-line-webapp/src/main/java/com/github/yukihane/samplelinewebapp/SampleLineWebappApplication.java
+++ line/sample-line-webapp/src/main/java/com/github/yukihane/samplelinewebapp/SampleLineWebappApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SampleLineWebappApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SampleLineWebappApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(SampleLineWebappApplication.class, args);
+    }
 
 }
