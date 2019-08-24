08936c2a8c7f50bed4ec0f56936d0bf85546c522
yukihane <yukihane.feather@gmail.com>
2019-08-25 08:44:50 +0900

    PostBackEventハンドラ追加

--- line/line-bot-sample/src/main/java/com/github/yukihane/linebotsample/controller/MyController.java
+++ line/line-bot-sample/src/main/java/com/github/yukihane/linebotsample/controller/MyController.java
@@ -13,32 +13,50 @@ import com.linecorp.bot.model.message.template.ButtonsTemplate;
 import com.linecorp.bot.model.message.template.Template;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoField;
 import java.util.List;
 
 @LineMessageHandler
 public class MyController {
 
     @EventMapping
-    public Message handleTextMessageEvent(final MessageEvent<TextMessageContent> event) throws Exception {
+    public TextMessage handleTextMessageEvent(final MessageEvent<TextMessageContent> event) throws Exception {
         System.out.println("event: " + event);
-        final String text = event.getMessage().getText();
-
-        final Action a1 = new URIAction("link", "https://qiita.com/", null);
-        final String data = "データです";
-        final PostbackAction a2 = new PostbackAction("postback", data);
-
-        final List<Action> actions = List.of(a1, a2);
-        final Template template = ButtonsTemplate.builder()
-            .title("タイトル")
-            .text("テキスト")
-            .actions(actions)
-            .build();
-        return new TemplateMessage(text, template);
+        return new TextMessage(event.getMessage().getText());
     }
 
     @EventMapping
     public Message handlePostBackEvent(final PostbackEvent event) throws Exception {
         final String data = event.getPostbackContent().getData();
+
+        if ("the-postback-action".equals(data)) {
+            final LocalDateTime now = LocalDateTime.now();
+            final int sec = now.get(ChronoField.SECOND_OF_MINUTE);
+
+            final Action a1 = new URIAction("link", "https://qiita.com/settings/account", null);
+
+            if (sec % 2 == 0) {
+                final List<Action> actions = List.of(a1);
+                final Template template = ButtonsTemplate.builder()
+                    .text("Go to qiita.com")
+                    .actions(actions)
+                    .build();
+                return new TemplateMessage("リンクです(" + now + ")", template);
+            } else {
+                final String d = "データです";
+                final PostbackAction a2 = new PostbackAction("postback", d);
+
+                final List<Action> actions = List.of(a1, a2);
+                final Template template = ButtonsTemplate.builder()
+                    .title("タイトル")
+                    .text("テキスト")
+                    .actions(actions)
+                    .build();
+                return new TemplateMessage("その他です(" + now + ")", template);
+            }
+
+        }
 
         return new TextMessage("PostBackEvent受信: " + data);
     }
