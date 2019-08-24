4dcdf7717c6a01dba5fffb9cf324b6e7c4497695
yukihane <yukihane.feather@gmail.com>
2019-08-25 01:46:54 +0900

    整形

--- line/line-bot-sample/src/main/java/com/github/yukihane/linebotsample/LineBotSampleApplication.java
+++ line/line-bot-sample/src/main/java/com/github/yukihane/linebotsample/LineBotSampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LineBotSampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LineBotSampleApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(LineBotSampleApplication.class, args);
+    }
 
 }
