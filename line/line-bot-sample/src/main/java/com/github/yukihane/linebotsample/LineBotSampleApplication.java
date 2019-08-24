ac03774732ba6187fe02fa4580a4973c701d582a
yukihane <yukihane.feather@gmail.com>
2019-08-25 01:43:28 +0900

    auto-generated files

---
+++ line/line-bot-sample/src/main/java/com/github/yukihane/linebotsample/LineBotSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.linebotsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineBotSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineBotSampleApplication.class, args);
+	}
+
+}
---
+++ line/line-bot-sample/src/test/java/com/github/yukihane/linebotsample/LineBotSampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.yukihane.linebotsample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LineBotSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
