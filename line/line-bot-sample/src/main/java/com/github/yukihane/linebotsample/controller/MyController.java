9b534c3940caddf249686199ce6aca614914e993
yukihane <yukihane.feather@gmail.com>
2019-08-25 07:37:45 +0900

    もう少し複雑なハンドラ

--- line/line-bot-sample/src/main/java/com/github/yukihane/linebotsample/controller/MyController.java
+++ line/line-bot-sample/src/main/java/com/github/yukihane/linebotsample/controller/MyController.java
@@ -1,17 +1,45 @@
 package com.github.yukihane.linebotsample.controller;
 
+import com.linecorp.bot.model.action.Action;
+import com.linecorp.bot.model.action.PostbackAction;
+import com.linecorp.bot.model.action.URIAction;
 import com.linecorp.bot.model.event.MessageEvent;
+import com.linecorp.bot.model.event.PostbackEvent;
 import com.linecorp.bot.model.event.message.TextMessageContent;
+import com.linecorp.bot.model.message.Message;
+import com.linecorp.bot.model.message.TemplateMessage;
 import com.linecorp.bot.model.message.TextMessage;
+import com.linecorp.bot.model.message.template.ButtonsTemplate;
+import com.linecorp.bot.model.message.template.Template;
 import com.linecorp.bot.spring.boot.annotation.EventMapping;
 import com.linecorp.bot.spring.boot.annotation.LineMessageHandler;
+import java.util.List;
 
 @LineMessageHandler
 public class MyController {
 
     @EventMapping
-    public TextMessage handleTextMessageEvent(final MessageEvent<TextMessageContent> event) throws Exception {
+    public Message handleTextMessageEvent(final MessageEvent<TextMessageContent> event) throws Exception {
         System.out.println("event: " + event);
-        return new TextMessage(event.getMessage().getText());
+        final String text = event.getMessage().getText();
+
+        final Action a1 = new URIAction("link", "https://qiita.com/", null);
+        final String data = "データです";
+        final PostbackAction a2 = new PostbackAction("postback", data);
+
+        final List<Action> actions = List.of(a1, a2);
+        final Template template = ButtonsTemplate.builder()
+            .title("タイトル")
+            .text("テキスト")
+            .actions(actions)
+            .build();
+        return new TemplateMessage(text, template);
+    }
+
+    @EventMapping
+    public Message handlePostBackEvent(final PostbackEvent event) throws Exception {
+        final String data = event.getPostbackContent().getData();
+
+        return new TextMessage("PostBackEvent受信: " + data);
     }
 }
