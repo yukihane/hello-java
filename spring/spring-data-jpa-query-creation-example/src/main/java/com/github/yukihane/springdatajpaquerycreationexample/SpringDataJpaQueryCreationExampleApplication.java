f9cfc9a92947df009f7c38a004999f7df047344c
yukihane <yukihane.feather@gmail.com>
2019-09-04 11:20:14 +0900

    整形

--- spring/spring-data-jpa-query-creation-example/src/main/java/com/github/yukihane/springdatajpaquerycreationexample/SpringDataJpaQueryCreationExampleApplication.java
+++ spring/spring-data-jpa-query-creation-example/src/main/java/com/github/yukihane/springdatajpaquerycreationexample/SpringDataJpaQueryCreationExampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDataJpaQueryCreationExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringDataJpaQueryCreationExampleApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringDataJpaQueryCreationExampleApplication.class, args);
+    }
 
 }
