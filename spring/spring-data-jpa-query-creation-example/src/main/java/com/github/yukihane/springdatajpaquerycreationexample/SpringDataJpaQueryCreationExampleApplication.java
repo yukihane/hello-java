a3181e57183d9b2c3110ae766fb20b09a415ca7b
yukihane <yukihane.feather@gmail.com>
2019-09-04 11:18:18 +0900

    auto-generated files

---
+++ spring/spring-data-jpa-query-creation-example/src/main/java/com/github/yukihane/springdatajpaquerycreationexample/SpringDataJpaQueryCreationExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.springdatajpaquerycreationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaQueryCreationExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaQueryCreationExampleApplication.class, args);
+	}
+
+}
