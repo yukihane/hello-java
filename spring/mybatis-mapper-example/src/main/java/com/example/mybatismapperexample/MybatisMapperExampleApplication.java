9eb48a16735fcb06ea64a1e561d7638f1b710ffe
yukihane <yukihane.feather@gmail.com>
2019-09-06 14:47:24 +0900

    auto-generated files

---
+++ spring/mybatis-mapper-example/src/main/java/com/example/mybatismapperexample/MybatisMapperExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mybatismapperexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisMapperExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MybatisMapperExampleApplication.class, args);
+	}
+
+}
