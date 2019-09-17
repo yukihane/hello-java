fdac25b433ddd9b721057bc2073bac82e0f3863b
yukihane <yukihane.feather@gmail.com>
2019-09-18 03:17:44 +0900

    line/sample-line-webapp をコピー

---
+++ spring/multiple-authorities-sample/src/main/java/com/github/yukihane/samplelinewebapp/SampleLineWebappApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.samplelinewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleLineWebappApplication {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SampleLineWebappApplication.class, args);
+    }
+
+}
---
+++ spring/multiple-authorities-sample/src/main/java/com/github/yukihane/samplelinewebapp/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.github.yukihane.samplelinewebapp.controller;
+
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyController {
+
+    @RequestMapping("/")
+    public OAuth2User welcome(@AuthenticationPrincipal final OAuth2User user) {
+        return user;
+    }
+}
