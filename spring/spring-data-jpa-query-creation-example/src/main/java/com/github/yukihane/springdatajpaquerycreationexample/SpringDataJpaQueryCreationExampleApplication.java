7915d82c7e8f9d23eeabfe6ba4933af459affd7f
yukihane <yukihane.feather@gmail.com>
2019-09-20 14:11:42 +0900

    in句で条件指定してdelete

--- spring/spring-data-jpa-query-creation-example/src/main/java/com/github/yukihane/springdatajpaquerycreationexample/SpringDataJpaQueryCreationExampleApplication.java
+++ spring/spring-data-jpa-query-creation-example/src/main/java/com/github/yukihane/springdatajpaquerycreationexample/SpringDataJpaQueryCreationExampleApplication.java
@@ -1,6 +1,7 @@
 package com.github.yukihane.springdatajpaquerycreationexample;
 
 import com.github.yukihane.springdatajpaquerycreationexample.entity.MyEntityRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -12,11 +13,12 @@ public class SpringDataJpaQueryCreationExampleApplication implements CommandLine
     @Autowired
     private MyEntityRepository repository;
 
-
     @Override
     public void run(final String... args) throws Exception {
         repository.findById("MY_ID");
         repository.deleteByMyEntityIdAndAltIdNot("ID", "ALT_ID");
+        repository.deleteByMyEntityIdIn(List.of("id1", "id2"));
+
     }
 
     public static void main(final String[] args) {
--- spring/spring-data-jpa-query-creation-example/src/main/java/com/github/yukihane/springdatajpaquerycreationexample/entity/MyEntityRepository.java
+++ spring/spring-data-jpa-query-creation-example/src/main/java/com/github/yukihane/springdatajpaquerycreationexample/entity/MyEntityRepository.java
@@ -1,9 +1,12 @@
 package com.github.yukihane.springdatajpaquerycreationexample.entity;
 
+import java.util.Collection;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface MyEntityRepository extends JpaRepository<MyEntity, String> {
     long deleteByMyEntityIdAndAltIdNot(String id, String altId);
+
+    long deleteByMyEntityIdIn(Collection<String> ids);
 }
