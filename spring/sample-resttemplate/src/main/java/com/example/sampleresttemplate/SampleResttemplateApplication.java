f9a34a55b0ecef28d1ca6bafac2805dd030b0640
yukihane <yukihane.feather@gmail.com>
2019-12-20 06:18:13 +0900

    auto-generated files

---
+++ spring/sample-resttemplate/src/main/java/com/example/sampleresttemplate/SampleResttemplateApplication.java
@@ -0,0 +1,13 @@
+package com.example.sampleresttemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleResttemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SampleResttemplateApplication.class, args);
+	}
+
+}
