31c210bcc92ae2e3ae6f40bc7969381e660baac4
yukihane <yukihane.feather@gmail.com>
2020-01-22 14:07:43 +0900

    406エラーの回避

--- spring/json-sjis-example/src/main/java/com/example/jsonsjisexample/JsonSjisExampleApplication.java
+++ spring/json-sjis-example/src/main/java/com/example/jsonsjisexample/JsonSjisExampleApplication.java
@@ -1,11 +1,16 @@
 package com.example.jsonsjisexample;
 
+import java.nio.charset.Charset;
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -40,7 +45,13 @@ public class JsonSjisExampleApplication implements ApplicationRunner {
 
     @Override
     public void run(final ApplicationArguments args) throws Exception {
-        final RestTemplate rt = restTemplateBuilder.build();
+        final MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+        converter.setSupportedMediaTypes(Arrays.asList(new MediaType(MediaType.TEXT_PLAIN, Charset.forName("Windows-31J"))));
+
+        final RestTemplate rt = restTemplateBuilder
+            .additionalMessageConverters(Arrays.asList(converter))
+            .build();
+
         final Greeting greeging = rt.getForObject("http://localhost:8080/", Greeting.class);
 
         log.info("greeting text: {}", greeging.getText());
