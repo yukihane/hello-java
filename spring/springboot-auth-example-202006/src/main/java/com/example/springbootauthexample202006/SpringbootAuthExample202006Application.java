b6913e8b56ad2a4b71915189e1b0a40d7aa19e5f
yukihane <yukihane.feather@gmail.com>
2020-06-12 14:24:42 +0900

    init

---
+++ spring/springboot-auth-example-202006/src/main/java/com/example/springbootauthexample202006/SpringbootAuthExample202006Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootauthexample202006;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAuthExample202006Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(SpringbootAuthExample202006Application.class, args);
+    }
+
+}
---
+++ spring/springboot-auth-example-202006/src/test/java/com/example/springbootauthexample202006/SpringbootAuthExample202006ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootauthexample202006;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootAuthExample202006ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
