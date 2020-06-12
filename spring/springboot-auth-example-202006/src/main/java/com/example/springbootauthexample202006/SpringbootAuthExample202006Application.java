9e63ec8ad42d45cabd066934ddeffc00e24ab335
yukihane <yukihane.feather@gmail.com>
2020-06-13 07:19:08 +0900

    UserDetailsServiceAutoConfiguration を disable

--- spring/springboot-auth-example-202006/src/main/java/com/example/springbootauthexample202006/SpringbootAuthExample202006Application.java
+++ spring/springboot-auth-example-202006/src/main/java/com/example/springbootauthexample202006/SpringbootAuthExample202006Application.java
@@ -2,8 +2,9 @@ package com.example.springbootauthexample202006;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class SpringbootAuthExample202006Application {
 
     public static void main(final String[] args) {
