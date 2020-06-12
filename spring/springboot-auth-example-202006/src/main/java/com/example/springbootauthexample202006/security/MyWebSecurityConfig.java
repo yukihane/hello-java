2692a5c1fc141d412777d3e9126c6f4f99727d87
yukihane <yukihane.feather@gmail.com>
2020-06-13 07:30:25 +0900

    pathにセキュリティ設定を行う

--- spring/springboot-auth-example-202006/src/main/java/com/example/springbootauthexample202006/security/MyWebSecurityConfig.java
+++ spring/springboot-auth-example-202006/src/main/java/com/example/springbootauthexample202006/security/MyWebSecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.springbootauthexample202006.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -11,5 +12,10 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(final HttpSecurity http) throws Exception {
         http.csrf().disable()
             .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/users/sign-up")
+            .permitAll()
+            .anyRequest().authenticated();
     }
 }
