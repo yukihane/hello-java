31312e8c94530bb6f6272d0b9c6c9607a83939ec
yukihane <yukihane.feather@gmail.com>
2020-06-13 07:25:50 +0900

    HttpSessionを利用しない

--- spring/springboot-auth-example-202006/src/main/java/com/example/springbootauthexample202006/security/MyWebSecurityConfig.java
+++ spring/springboot-auth-example-202006/src/main/java/com/example/springbootauthexample202006/security/MyWebSecurityConfig.java
@@ -3,11 +3,13 @@ package com.example.springbootauthexample202006.security;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @Configuration
 public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
-        http.csrf().disable();
+        http.csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 }
