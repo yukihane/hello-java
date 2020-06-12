ee8d3baf266e929b96f51f29ffecdd444e3fed86
yukihane <yukihane.feather@gmail.com>
2020-06-12 16:23:25 +0900

    web security confg の auto-configuration を無効化

---
+++ spring/springboot-auth-example-202006/src/main/java/com/example/springbootauthexample202006/security/MyWebSecurityConfig.java
@@ -0,0 +1,12 @@
+package com.example.springbootauthexample202006.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(final HttpSecurity http) throws Exception {
+    }
+}
