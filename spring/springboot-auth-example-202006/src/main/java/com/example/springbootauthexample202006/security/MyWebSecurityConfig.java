62238f5aaafc0c88c05ad66666229c2a12d2ea42
yukihane <yukihane.feather@gmail.com>
2020-06-12 16:46:36 +0900

    CSRFをdisable

--- spring/springboot-auth-example-202006/src/main/java/com/example/springbootauthexample202006/security/MyWebSecurityConfig.java
+++ spring/springboot-auth-example-202006/src/main/java/com/example/springbootauthexample202006/security/MyWebSecurityConfig.java
@@ -8,5 +8,6 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
+        http.csrf().disable();
     }
 }
