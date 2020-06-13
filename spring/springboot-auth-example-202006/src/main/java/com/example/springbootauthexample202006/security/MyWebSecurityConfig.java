973f5f7a33b84ffbc2f9a069c0a9bd0b9393395c
yukihane <yukihane.feather@gmail.com>
2020-06-13 11:39:36 +0900

    認証処理のconfiguration

--- spring/springboot-auth-example-202006/src/main/java/com/example/springbootauthexample202006/security/MyWebSecurityConfig.java
+++ spring/springboot-auth-example-202006/src/main/java/com/example/springbootauthexample202006/security/MyWebSecurityConfig.java
@@ -1,13 +1,25 @@
 package com.example.springbootauthexample202006.security;
 
+import com.example.springbootauthexample202006.user.ApplicationUserRepository;
+import java.util.Arrays;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
+@RequiredArgsConstructor
 public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final ApplicationUserRepository applicationUserRepository;
+
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
         http.csrf().disable()
@@ -17,5 +29,12 @@ public class MyWebSecurityConfig extends WebSecurityConfigurerAdapter {
             .antMatchers(HttpMethod.POST, "/users/sign-up")
             .permitAll()
             .anyRequest().authenticated();
+
+        final PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        final AuthenticationProvider provider = new ApplicationUserAuthenticationProvider(passwordEncoder,
+            applicationUserRepository);
+        final AuthenticationManager manager = new ProviderManager(Arrays.asList(provider));
+
+        http.addFilter(new JWTAuthenticationFilter(manager));
     }
 }
