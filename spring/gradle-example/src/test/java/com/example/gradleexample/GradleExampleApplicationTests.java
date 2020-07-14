f019900bc62c80aa2fc423f607780ddbe55f0042
yukihane <yukihane.feather@gmail.com>
2020-07-15 01:19:01 +0900

    init

---
+++ spring/gradle-example/src/test/java/com/example/gradleexample/GradleExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gradleexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GradleExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
