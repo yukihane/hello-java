b04044fa870f4b15fe35b5261fb45d094aa3dd24
yukihane <yukihane.feather@gmail.com>
2020-07-29 05:21:56 +0900

    UserDetailsServiceAutoConfiguration 無効化 https://yukihane.github.io/blog/202006/13/userdetailsservice-samples-considered-harmful-for-biginners/

--- spring/tettei-spring-example/src/main/java/com/example/tetteispringexample/TetteiSpringExampleApplication.java
+++ spring/tettei-spring-example/src/main/java/com/example/tetteispringexample/TetteiSpringExampleApplication.java
@@ -2,12 +2,11 @@ package com.example.tetteispringexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = UserDetailsServiceAutoConfiguration.class)
 public class TetteiSpringExampleApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(TetteiSpringExampleApplication.class, args);
-	}
-
+    public static void main(final String[] args) {
+        SpringApplication.run(TetteiSpringExampleApplication.class, args);
+    }
 }
