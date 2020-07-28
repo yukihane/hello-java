8cdd5ebf19f5406d59e51dfe8fa6d7601bfef049
yukihane <yukihane.feather@gmail.com>
2020-07-29 08:24:35 +0900

    認証処理を実装

--- spring/tettei-spring-example/src/main/java/com/example/tetteispringexample/config/WebSecurityConfig.java
+++ spring/tettei-spring-example/src/main/java/com/example/tetteispringexample/config/WebSecurityConfig.java
@@ -1,13 +1,31 @@
 package com.example.tetteispringexample.config;
 
+import com.example.tetteispringexample.security.UserAuthentication;
+import com.example.tetteispringexample.user.domain.User;
+import com.example.tetteispringexample.user.repository.UserRepository;
+import java.util.Arrays;
+import java.util.Optional;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@RequiredArgsConstructor
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final UserRepository userRepository;
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
@@ -23,5 +41,42 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .failureUrl(
                 "/loginForm?error=true")
             .permitAll();
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        final PasswordEncoder passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        return new ProviderManager(Arrays.asList(new MyAuthenticationProvider(passwordEncoder, userRepository)));
+    }
+
+    @RequiredArgsConstructor
+    public static class MyAuthenticationProvider implements AuthenticationProvider {
+
+        private final PasswordEncoder passwordEncoder;
+        private final UserRepository userRepository;
+
+        @Override
+        public Authentication authenticate(final Authentication authentication) throws AuthenticationException {
+            final UsernamePasswordAuthenticationToken authn = (UsernamePasswordAuthenticationToken) authentication;
+            final String username = (String) authn.getPrincipal();
+            final String password = (String) authn.getCredentials();
+
+            final Optional<User> user = userRepository.findById(username);
+
+            final Optional<UserAuthentication> result = user.map(u -> {
+                if (passwordEncoder.matches(password, u.getPassword())) {
+                    return new UserAuthentication(username);
+                } else {
+                    return null;
+                }
+            });
+
+            return result.orElseThrow(() -> new BadCredentialsException("illegal username or password"));
+        }
+
+        @Override
+        public boolean supports(final Class<?> authentication) {
+            return UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication);
+        }
     }
 }
---
+++ spring/tettei-spring-example/src/main/resources/data.sql
@@ -0,0 +1,2 @@
+INSERT INTO usr (user_id, first_name, last_name, role_name, password)
+VALUES ('taro-yamada', '太郎', '山田', 'USER', '{noop}yamada');
