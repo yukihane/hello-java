09c9559213e142f1ff9b9910de372f2c7fd110e0
yukihane <yukihane.feather@gmail.com>
2020-07-29 08:38:53 +0900

    コーディング標準に準拠させる

--- spring/tettei-spring-example/src/main/java/com/example/tetteispringexample/room/presentation/RoomsController.java
+++ spring/tettei-spring-example/src/main/java/com/example/tetteispringexample/room/presentation/RoomsController.java
@@ -1,33 +1,33 @@
 package com.example.tetteispringexample.room.presentation;
 
-
 import com.example.tetteispringexample.room.domain.ReservableRoom;
 import com.example.tetteispringexample.room.domain.RoomService;
 import java.time.LocalDate;
 import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 @RequestMapping("rooms")
+@RequiredArgsConstructor
 public class RoomsController {
-    @Autowired
-    RoomService roomService;
 
-    @RequestMapping(value = "{date}", method = RequestMethod.GET)
-    String listRooms(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") final LocalDate date, final Model model) {
+    private final RoomService roomService;
+
+    @GetMapping("{date}")
+    String listRooms(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") final LocalDate date,
+        final Model model) {
         final List<ReservableRoom> rooms = roomService.findReservableRooms(date);
         model.addAttribute("rooms", rooms);
         return "room/listRooms";
     }
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     String listRooms(final Model model) {
         final LocalDate today = LocalDate.now();
         model.addAttribute("date", today);
