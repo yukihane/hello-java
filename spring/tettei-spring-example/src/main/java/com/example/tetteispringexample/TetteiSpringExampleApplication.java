b9b6de6289d47590c32ba382ce571c9dcf663044
yukihane <yukihane.feather@gmail.com>
2020-07-29 04:07:25 +0900

    init

---
+++ spring/tettei-spring-example/src/main/java/com/example/tetteispringexample/TetteiSpringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tetteispringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TetteiSpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TetteiSpringExampleApplication.class, args);
+	}
+
+}
