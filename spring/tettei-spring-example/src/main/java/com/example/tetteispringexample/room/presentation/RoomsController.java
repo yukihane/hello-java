ffa6fdfaaf8bb6552aa52145b55c7bd120ef84de
yukihane <yukihane.feather@gmail.com>
2020-07-30 04:41:58 +0900

    debug logging

--- spring/tettei-spring-example/src/main/java/com/example/tetteispringexample/room/presentation/RoomsController.java
+++ spring/tettei-spring-example/src/main/java/com/example/tetteispringexample/room/presentation/RoomsController.java
@@ -5,6 +5,7 @@ import com.example.tetteispringexample.room.domain.RoomService;
 import java.time.LocalDate;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("rooms")
 @RequiredArgsConstructor
+@Slf4j
 public class RoomsController {
 
     private final RoomService roomService;
@@ -23,6 +25,8 @@ public class RoomsController {
     String listRooms(@DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @PathVariable("date") final LocalDate date,
         final Model model) {
         final List<ReservableRoom> rooms = roomService.findReservableRooms(date);
+        log.info("Reservable Rooms({}): {}", rooms.size(), rooms);
+
         model.addAttribute("rooms", rooms);
         return "room/listRooms";
     }
