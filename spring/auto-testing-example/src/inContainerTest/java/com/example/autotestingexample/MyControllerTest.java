47613a866b02df473de73e20153d183984277634
yukihane <yukihane.feather@gmail.com>
2020-08-17 08:40:24 +0900

    モックオブジェクトを操作する

--- spring/auto-testing-example/src/inContainerTest/java/com/example/autotestingexample/MyControllerTest.java
+++ spring/auto-testing-example/src/inContainerTest/java/com/example/autotestingexample/MyControllerTest.java
@@ -1,5 +1,7 @@
 package com.example.autotestingexample;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -9,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
 /**
  * @see https://docs.spring.io/spring-boot/docs/current/reference/htmlsingle/#boot-features-testing-spring-boot-applications-testing-autoconfigured-mvc-tests
@@ -28,7 +31,18 @@ class MyControllerTest {
     @WithMockUser
     @Test
     void ログイン済み() throws Exception {
-        mvc.perform(get("/")).andExpect(status().isOk());
+        when(myService.getNow()).thenReturn("2020-01-01T10:10:10");
+        final MyTable obj = new MyTable();
+        obj.setId(10);
+        obj.setMessage("2020-02-02T02:02:02");
+        when(myMapper.select()).thenReturn(obj);
+
+        final MvcResult result = mvc.perform(get("/"))
+            .andExpect(status().isOk()).andReturn();
+        final String content = result.getResponse().getContentAsString();
+
+        assertThat(content).contains("MyTable(id=10, message=2020-02-02T02:02:02)");
+        assertThat(content).contains("2020-01-01T10:10:10");
     }
 
     @Test
