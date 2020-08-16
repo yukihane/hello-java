b381e9b98c68bfdfc2e50ae6ad76fe89ba0f7c63
yukihane <yukihane.feather@gmail.com>
2020-08-16 18:33:43 +0900

    init

---
+++ spring/auto-testing-example/src/main/java/com/example/autotestingexample/AutoTestingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.autotestingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTestingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoTestingExampleApplication.class, args);
+	}
+
+}
---
+++ spring/auto-testing-example/src/test/java/com/example/autotestingexample/AutoTestingExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.autotestingexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutoTestingExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
