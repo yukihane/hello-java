8cdbbabd95a024148fd064de2ebb8e28f492de99
yukihane <yukihane.feather@gmail.com>
2020-08-16 18:43:45 +0900

    Spring Security auto-configurationを一旦disable

--- spring/auto-testing-example/src/main/java/com/example/autotestingexample/AutoTestingExampleApplication.java
+++ spring/auto-testing-example/src/main/java/com/example/autotestingexample/AutoTestingExampleApplication.java
@@ -2,12 +2,13 @@ package com.example.autotestingexample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class AutoTestingExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AutoTestingExampleApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(AutoTestingExampleApplication.class, args);
+    }
 
 }
