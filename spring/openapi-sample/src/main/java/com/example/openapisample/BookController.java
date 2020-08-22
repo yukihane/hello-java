9b06d2a31c04f8957a7439fd50ba184943c566f7
yukihane <yukihane.feather@gmail.com>
2020-08-22 12:40:34 +0900

    post実装

--- spring/openapi-sample/src/main/java/com/example/openapisample/BookController.java
+++ spring/openapi-sample/src/main/java/com/example/openapisample/BookController.java
@@ -1,9 +1,12 @@
 package com.example.openapisample;
 
 import java.util.List;
+import java.util.stream.Collectors;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.openapi.example.api.BooksApi;
 import org.openapi.example.model.BookListModel;
+import org.openapi.example.model.BookModel;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,10 +21,17 @@ public class BookController implements BooksApi {
     @Override
     public ResponseEntity<BookListModel> booksGet() {
         final List<Book> books = bookRepository.findAll();
-        books.stream().map(bookMapper::entityToModel);
+        final List<BookModel> res = books.stream().map(bookMapper::entityToModel).collect(Collectors.toList());
         final BookListModel output = new BookListModel();
-        output.books(List.of());
+        output.books(res);
         return new ResponseEntity<>(output, HttpStatus.OK);
     }
 
+    @Override
+    public ResponseEntity<BookModel> booksPost(@Valid final BookModel bookModel) {
+        final Book book = bookMapper.modelToEntity(bookModel);
+        final Book saved = bookRepository.save(book);
+        final BookModel savedModel = bookMapper.entityToModel(saved);
+        return new ResponseEntity<>(savedModel, HttpStatus.OK);
+    }
 }
