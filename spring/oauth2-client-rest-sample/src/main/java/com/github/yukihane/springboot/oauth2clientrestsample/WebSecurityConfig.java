3b9484b5e8f881ad28a41837cd8105514f9a6383
yukihane <yukihane.feather@gmail.com>
2020-09-02 04:12:33 +0900

    postできるように一旦csrfをoffに

--- spring/oauth2-client-rest-sample/src/main/java/com/github/yukihane/springboot/oauth2clientrestsample/WebSecurityConfig.java
+++ spring/oauth2-client-rest-sample/src/main/java/com/github/yukihane/springboot/oauth2clientrestsample/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.github.yukihane.springboot.oauth2clientrestsample;
 
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -9,6 +10,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(final HttpSecurity http) throws Exception {
+        // POSTできるように一旦解除しておく
         http.csrf().disable();
+
+        // https://yukihane.github.io/blog/202007/21/hello-oidc-with-keycloak/
+        // OAuth2WebSecurityConfiguration 設定をそのままコピー
+        http.authorizeRequests((requests) -> requests.anyRequest().authenticated());
+        http.oauth2Login(Customizer.withDefaults());
+        http.oauth2Client();
+
     }
 }
