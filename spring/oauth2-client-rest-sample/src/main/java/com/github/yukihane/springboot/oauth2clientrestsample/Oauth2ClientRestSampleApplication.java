616db10c57247ebe8bfacea8dc699d2f789d5a9d
yukihane <yukihane.feather@gmail.com>
2020-09-02 02:20:38 +0900

    init

---
+++ spring/oauth2-client-rest-sample/src/main/java/com/github/yukihane/springboot/oauth2clientrestsample/Oauth2ClientRestSampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.springboot.oauth2clientrestsample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2ClientRestSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2ClientRestSampleApplication.class, args);
+	}
+
+}
---
+++ spring/oauth2-client-rest-sample/src/test/java/com/github/yukihane/springboot/oauth2clientrestsample/Oauth2ClientRestSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.springboot.oauth2clientrestsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ClientRestSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
