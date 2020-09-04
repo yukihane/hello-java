073481e7ef0a02e43e2e44d4acd2567dc8eeeeb2
yukihane <yukihane.feather@gmail.com>
2020-09-04 09:31:28 +0900

    取得した情報を表示

--- spring/oidc/oidc-add-scope/src/main/java/com/example/oidcexample/HelloController.java
+++ spring/oidc/oidc-add-scope/src/main/java/com/example/oidcexample/HelloController.java
@@ -3,6 +3,8 @@ package com.example.oidcexample;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,9 @@ public class HelloController {
     public String index() {
         final Authentication authn = SecurityContextHolder.getContext().getAuthentication();
         log.info("Authentication: {}", authn);
-        return "Hello " + authn.getName() + "!";
+
+        final OAuth2User user = ((OAuth2AuthenticationToken) authn).getPrincipal();
+        final Object favNum = user.getAttribute("fav-number");
+        return "Hello " + authn.getName() + "! Your fav is " + favNum + ".";
     }
 }
