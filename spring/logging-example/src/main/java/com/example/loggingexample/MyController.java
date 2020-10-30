3c08471602150a693dd185c77d974663fb7d407b
yukihane <yukihane.feather@gmail.com>
2020-10-30 14:26:27 +0900

    JSON形式でログ出力

--- spring/logging-example/src/main/java/com/example/loggingexample/MyController.java
+++ spring/logging-example/src/main/java/com/example/loggingexample/MyController.java
@@ -1,13 +1,19 @@
 package com.example.loggingexample;
 
+import static net.logstash.logback.argument.StructuredArguments.v;
+
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController("/")
+@Slf4j
 public class MyController {
 
     @GetMapping
     public String index() {
+        log.info("This is info message");
+        log.info("JSON Log", v("Hello", "Japan"), v("Hello", "World"));
         return "{}";
     }
 
