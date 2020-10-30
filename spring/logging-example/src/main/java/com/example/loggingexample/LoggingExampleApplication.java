50d4a40ae61350a97b493bc02cb4979db6b48b9b
yukihane <yukihane.feather@gmail.com>
2020-10-30 13:10:04 +0900

    init

---
+++ spring/logging-example/src/main/java/com/example/loggingexample/LoggingExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.loggingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggingExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoggingExampleApplication.class, args);
+	}
+
+}
