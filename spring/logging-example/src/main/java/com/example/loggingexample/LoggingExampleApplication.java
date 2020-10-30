243187815fbc4afdb7972068c51b7c40a0710bad
yukihane <yukihane.feather@gmail.com>
2020-10-30 13:22:16 +0900

    sample controller

--- spring/logging-example/src/main/java/com/example/loggingexample/LoggingExampleApplication.java
+++ spring/logging-example/src/main/java/com/example/loggingexample/LoggingExampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LoggingExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LoggingExampleApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(LoggingExampleApplication.class, args);
+    }
 
 }
