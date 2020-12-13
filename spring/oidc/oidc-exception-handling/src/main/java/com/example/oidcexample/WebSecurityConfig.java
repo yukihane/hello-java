463dc31752a91ac69b7617b84fe5f59f5818c06c
yukihane <yukihane.feather@gmail.com>
2020-12-14 07:38:36 +0900

    一旦commit

---
+++ spring/oidc/oidc-exception-handling/src/main/java/com/example/oidcexample/WebSecurityConfig.java
@@ -0,0 +1,9 @@
+package com.example.oidcexample;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+//@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
