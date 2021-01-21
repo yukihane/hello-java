7dbe76a09b88cf9464154c48cc6822bbb233676c
yukihane <yukihane.feather@gmail.com>
2021-01-22 01:33:08 +0900

    init

---
+++ spring/oidc/oauth2-state-save/src/main/java/com/example/oauth2statesave/Oauth2StateSaveApplication.java
@@ -0,0 +1,13 @@
+package com.example.oauth2statesave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2StateSaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2StateSaveApplication.class, args);
+	}
+
+}
---
+++ spring/oidc/oauth2-state-save/src/test/java/com/example/oauth2statesave/Oauth2StateSaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oauth2statesave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2StateSaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
