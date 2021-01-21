e5d73b2308e8800a2828a56d1cf3b6cdd7662f4b
yukihane <yukihane.feather@gmail.com>
2021-01-22 08:44:40 +0900

    本質に無関係なコードを除去

--- spring/oidc/oauth2-state-save/src/main/java/com/example/oauth2statesave/SecureController.java
+++ spring/oidc/oauth2-state-save/src/main/java/com/example/oauth2statesave/SecureController.java
@@ -1,6 +1,5 @@
 package com.example.oauth2statesave;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,20 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/secure")
-@Slf4j
 public class SecureController {
 
     @GetMapping("hello")
     public String hello() {
         final Authentication authn = SecurityContextHolder.getContext().getAuthentication();
-        log.info("Authentication: {}", authn);
         return "Hello " + authn.getName() + "!";
     }
 
     @GetMapping("bye")
     public String bye() {
         final Authentication authn = SecurityContextHolder.getContext().getAuthentication();
-        log.info("Authentication: {}", authn);
         return "Bye " + authn.getName() + "!";
     }
 }
