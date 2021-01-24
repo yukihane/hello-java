46d6fd9107ed570d8b3ea1f2335e9be907b05315
yukihane <yukihane.feather@gmail.com>
2021-01-24 14:03:02 +0900

    CookieでなくヘッダでセッションIDを連携する

--- spring/oidc/oauth2-state-save/src/main/java/com/example/oauth2statesave/SpringHttpSessionConfig.java
+++ spring/oidc/oauth2-state-save/src/main/java/com/example/oauth2statesave/SpringHttpSessionConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.session.MapSessionRepository;
 import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
+import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
+import org.springframework.session.web.http.HttpSessionIdResolver;
 
 // https://docs.spring.io/spring-session/docs/current/reference/html5/#api-enablespringhttpsession
 @EnableSpringHttpSession
@@ -14,5 +16,10 @@ public class SpringHttpSessionConfig {
     @Bean
     public MapSessionRepository sessionRepository() {
         return new MapSessionRepository(new ConcurrentHashMap<>());
+    }
+
+    @Bean
+    public HttpSessionIdResolver httpSessionIdResolver() {
+        return HeaderHttpSessionIdResolver.xAuthToken();
     }
 }
