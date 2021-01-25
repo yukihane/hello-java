8b87a5bd17b93c4f064731f328e3b0664e14aa22
yukihane <yukihane.feather@gmail.com>
2021-01-26 04:07:59 +0900

    ガバガバIdPに接続するためのRP

---
+++ spring/oidc/oidc-add-scope/src/main/java/com/example/oidcexample/GabaIdpConfig.java
@@ -0,0 +1,32 @@
+package com.example.oidcexample;
+
+import java.util.Set;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.oauth2.client.oidc.authentication.OidcIdTokenDecoderFactory;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserService;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoderFactory;
+
+@Profile("gabaidp")
+@Configuration
+public class GabaIdpConfig {
+
+    @Bean
+    public JwtDecoderFactory<ClientRegistration> idTokenDecoderFactory() {
+        final OidcIdTokenDecoderFactory idTokenDecoderFactory = new OidcIdTokenDecoderFactory();
+        idTokenDecoderFactory.setJwsAlgorithmResolver(clientRegistration -> {
+            return MacAlgorithm.HS256;
+        });
+        return idTokenDecoderFactory;
+    }
+
+    @Bean
+    OidcUserService oidcUserService() {
+        final OidcUserService ret = new OidcUserService();
+        ret.setAccessibleScopes(Set.of());
+        return ret;
+    }
+}
