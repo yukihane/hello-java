2c4591fd95bce744ae208133bdc78b0a29f299ed
yukihane <yukihane.feather@gmail.com>
2021-02-09 05:43:36 +0900

    init

---
+++ spring/cors/cors-sample/src/test/java/com/example/corssample/CorsSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.corssample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorsSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
