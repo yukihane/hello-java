8c52753d64ae895accff4eb828c1cbc8c36828bc
yukihane <yukihane.feather@gmail.com>
2021-04-15 02:40:26 +0900

    init

---
+++ spring/errorcontroller-throw-exception-example/src/main/java/com/example/errorcontrollerthrowexceptionexample/ErrorcontrollerThrowExceptionExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.errorcontrollerthrowexceptionexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErrorcontrollerThrowExceptionExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErrorcontrollerThrowExceptionExampleApplication.class, args);
+	}
+
+}
---
+++ spring/errorcontroller-throw-exception-example/src/test/java/com/example/errorcontrollerthrowexceptionexample/ErrorcontrollerThrowExceptionExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.errorcontrollerthrowexceptionexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErrorcontrollerThrowExceptionExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
