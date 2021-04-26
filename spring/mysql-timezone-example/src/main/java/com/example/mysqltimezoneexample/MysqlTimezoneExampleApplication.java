c1a084cfe00ba8b252941c728c6415365320815c
yukihane <yukihane.feather@gmail.com>
2021-04-27 02:22:46 +0900

    init

---
+++ spring/mysql-timezone-example/src/main/java/com/example/mysqltimezoneexample/MysqlTimezoneExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.mysqltimezoneexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MysqlTimezoneExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MysqlTimezoneExampleApplication.class, args);
+	}
+
+}
---
+++ spring/mysql-timezone-example/src/test/java/com/example/mysqltimezoneexample/MysqlTimezoneExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mysqltimezoneexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MysqlTimezoneExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
