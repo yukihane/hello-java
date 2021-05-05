bbe79f91803769f871ce1f4a11cc4634c28372f7
yukihane <yukihane.feather@gmail.com>
2021-05-06 06:11:14 +0900

    以下の問題の暫定対応: https://github.com/spring-projects/spring-security/issues/9494

--- line/sample-line-webapp/src/main/java/com/github/yukihane/samplelinewebapp/SampleLineWebappApplication.java
+++ line/sample-line-webapp/src/main/java/com/github/yukihane/samplelinewebapp/SampleLineWebappApplication.java
@@ -2,6 +2,11 @@ package com.github.yukihane.samplelinewebapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.oauth2.client.oidc.authentication.OidcIdTokenDecoderFactory;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.jose.jws.MacAlgorithm;
+import org.springframework.security.oauth2.jwt.JwtDecoderFactory;
 
 @SpringBootApplication
 public class SampleLineWebappApplication {
@@ -10,4 +15,10 @@ public class SampleLineWebappApplication {
         SpringApplication.run(SampleLineWebappApplication.class, args);
     }
 
+    @Bean
+    public JwtDecoderFactory<ClientRegistration> idTokenDecoderFactory() {
+        final OidcIdTokenDecoderFactory idTokenDecoderFactory = new OidcIdTokenDecoderFactory();
+        idTokenDecoderFactory.setJwsAlgorithmResolver(clientRegistration -> MacAlgorithm.HS256);
+        return idTokenDecoderFactory;
+    }
 }
