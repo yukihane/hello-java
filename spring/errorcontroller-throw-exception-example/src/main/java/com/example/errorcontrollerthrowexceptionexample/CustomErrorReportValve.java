c7030ae92bd941bde99f058063b5aa2a7b40514a
yukihane <yukihane.feather@gmail.com>
2021-05-21 05:51:32 +0900

    ErrorReportValve を参考に実装を見直し。 (従来の実装だと 302 リダイレクトなどでも通ってしまう)

--- spring/errorcontroller-throw-exception-example/src/main/java/com/example/errorcontrollerthrowexceptionexample/CustomErrorReportValve.java
+++ spring/errorcontroller-throw-exception-example/src/main/java/com/example/errorcontrollerthrowexceptionexample/CustomErrorReportValve.java
@@ -1,12 +1,14 @@
 package com.example.errorcontrollerthrowexceptionexample;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
 import org.apache.catalina.connector.Request;
 import org.apache.catalina.connector.Response;
 import org.apache.catalina.valves.ErrorReportValve;
+import org.apache.coyote.ActionCode;
+import org.apache.tomcat.util.ExceptionUtils;
 
 public class CustomErrorReportValve extends ErrorReportValve {
 
@@ -14,21 +16,46 @@ public class CustomErrorReportValve extends ErrorReportValve {
     Logger log = Logger.getLogger(CustomErrorReportValve.class.getName());
 
     @Override
-    protected void report(final Request request, final Response response, final Throwable t) {
-        try {
-            // Write a more friendly, less technical message to the user
-            final BufferedWriter out = new BufferedWriter(new OutputStreamWriter(response.getOutputStream()));
-            out.write("<html><head><title>Oops</title><body>");
-            out.write("<h1>Oops</h1>");
-            out.write("<p>Well, that didn't go as we had expected.</p>");
-            out.write("<p>Don't worry though, we're working on it.</p>");
-            out.write("</body></html>");
-            out.close();
+    protected void report(final Request request, final Response response, final Throwable throwable) {
+        // ErrorReportValve を参考に実装
 
-            // Log the error with your favorite logging framework...
-            log.severe("Uncaught throwable was thrown: " + t.getMessage());
-        } catch (final IOException e) {
-            e.printStackTrace();
+        final int statusCode = response.getStatus();
+
+        // Do nothing on a 1xx, 2xx and 3xx status
+        // Do nothing if anything has been written already
+        // Do nothing if the response hasn't been explicitly marked as in error
+        //    and that error has not been reported.
+        if (statusCode < 400 || response.getContentWritten() > 0 || !response.setErrorReported()) {
+            return;
+        }
+
+        // If an error has occurred that prevents further I/O, don't waste time
+        // producing an error report that will never be read
+        final AtomicBoolean result = new AtomicBoolean(false);
+        response.getCoyoteResponse().action(ActionCode.IS_IO_ALLOWED, result);
+        if (!result.get()) {
+            return;
+        }
+
+        try {
+            try {
+                response.setContentType("text/html");
+                response.setCharacterEncoding("utf-8");
+            } catch (final Throwable t) {
+                ExceptionUtils.handleThrowable(t);
+                if (container.getLogger().isDebugEnabled()) {
+                    container.getLogger().debug("status.setContentType", t);
+                }
+            }
+            final Writer writer = response.getReporter();
+            if (writer != null) {
+                // If writer is null, it's an indication that the response has
+                // been hard committed already, which should never happen
+                writer.write("<!doctype html><html lang=\"en\"><title>error</title><body>Error occured.</body></html>");
+                response.finishResponse();
+            }
+        } catch (IOException | IllegalStateException e) {
+            // Ignore
         }
     }
 }
