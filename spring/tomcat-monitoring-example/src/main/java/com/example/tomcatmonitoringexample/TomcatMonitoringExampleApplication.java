c056f0f5de81689cbe3de061ff26c21d67ab0919
yukihane <yukihane.feather@gmail.com>
2021-06-12 17:32:17 +0900

    init

---
+++ spring/tomcat-monitoring-example/src/main/java/com/example/tomcatmonitoringexample/TomcatMonitoringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.tomcatmonitoringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TomcatMonitoringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TomcatMonitoringExampleApplication.class, args);
+	}
+
+}
---
+++ spring/tomcat-monitoring-example/src/test/java/com/example/tomcatmonitoringexample/TomcatMonitoringExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tomcatmonitoringexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TomcatMonitoringExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
