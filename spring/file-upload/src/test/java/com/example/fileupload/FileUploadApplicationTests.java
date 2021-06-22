b8f5186da2a826ea36b604aa5f59701d282a9f70
yukihane <yukihane.feather@gmail.com>
2021-06-23 03:14:50 +0900

    init

---
+++ spring/file-upload/src/test/java/com/example/fileupload/FileUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fileupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
