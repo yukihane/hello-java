d78e2cfbac2f013234d5350ce0dc94cb6148e6e4
yukihane <yukihane.feather@gmail.com>
2021-07-10 08:02:34 +0900

    auto-generated files

---
+++ spring/with-ts/src/main/java/com/github/yukihane/withts/WithTsApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.withts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithTsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithTsApplication.class, args);
+	}
+
+}
---
+++ spring/with-ts/src/test/java/com/github/yukihane/withts/WithTsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.withts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithTsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
