d8d667635ccada426bfcde817ca28eb31fe9f757
yukihane <yukihane.feather@gmail.com>
2021-07-10 21:52:24 +0900

    auto-generated files

---
+++ spring/with-ts-babel/src/main/java/com/github/yukihane/withtsbabel/WithTsBabelApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.withtsbabel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithTsBabelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WithTsBabelApplication.class, args);
+	}
+
+}
---
+++ spring/with-ts-babel/src/test/java/com/github/yukihane/withtsbabel/WithTsBabelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.withtsbabel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithTsBabelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
