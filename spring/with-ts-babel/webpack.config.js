7fd4ac4db9f7d3b706555e31eb55cefb0a5c9d19
yukihane <yukihane.feather@gmail.com>
2021-07-11 12:28:19 +0900

    with-ts プロジェクトからコピー

---
+++ spring/with-ts-babel/webpack.config.js
@@ -0,0 +1,42 @@
+"use strict";
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
+
+const path = require("path");
+
+const isProduction = process.env.NODE_ENV == "production";
+
+const config = {
+  entry: "./src/main/js/index.ts",
+  output: {
+    path: path.resolve(__dirname, "build/resources/main/static"),
+  },
+  plugins: [
+    // Add your plugins here
+    // Learn more about plugins from https://webpack.js.org/configuration/plugins/
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)$/i,
+        loader: "ts-loader",
+        exclude: ["/node_modules/"],
+      },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+};
+
+module.exports = () => {
+  if (isProduction) {
+    config.mode = "production";
+  } else {
+    config.mode = "development";
+    config.devtool = "inline-source-map";
+  }
+  return config;
+};
