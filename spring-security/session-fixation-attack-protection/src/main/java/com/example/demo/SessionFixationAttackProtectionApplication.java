34ed6735fa3c1283d68d5fad8514d07732178224
yukihane <yukihane.feather@gmail.com>
2021-09-23 12:50:30 +0900

    init

---
+++ spring-security/session-fixation-attack-protection/src/main/java/com/example/demo/SessionFixationAttackProtectionApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SessionFixationAttackProtectionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SessionFixationAttackProtectionApplication.class, args);
+    }
+
+}
