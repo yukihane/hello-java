8456bda8837890e621875da4a88074c02a0f20d2
yukihane <yukihane.feather@gmail.com>
2021-10-17 07:39:10 +0900

    整形

--- spring/mapstruct-spring-extensions-example/src/main/java/com/github/yukihane/spring/mapstructspringextensionsexample/MapstructSpringExtensionsExampleApplication.java
+++ spring/mapstruct-spring-extensions-example/src/main/java/com/github/yukihane/spring/mapstructspringextensionsexample/MapstructSpringExtensionsExampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MapstructSpringExtensionsExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MapstructSpringExtensionsExampleApplication.class, args);
-	}
+    public static void main(final String[] args) {
+        SpringApplication.run(MapstructSpringExtensionsExampleApplication.class, args);
+    }
 
 }
