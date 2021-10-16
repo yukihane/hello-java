6570d3d41d786be623785ce71b3871597d66d2af
yukihane <yukihane.feather@gmail.com>
2021-10-17 07:36:35 +0900

    init

---
+++ spring/mapstruct-spring-extensions-example/src/main/java/com/github/yukihane/spring/mapstructspringextensionsexample/MapstructSpringExtensionsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.spring.mapstructspringextensionsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapstructSpringExtensionsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapstructSpringExtensionsExampleApplication.class, args);
+	}
+
+}
---
+++ spring/mapstruct-spring-extensions-example/src/test/java/com/github/yukihane/spring/mapstructspringextensionsexample/MapstructSpringExtensionsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.yukihane.spring.mapstructspringextensionsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapstructSpringExtensionsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
