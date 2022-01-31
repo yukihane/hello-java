2cb591d870d2236a294f73c8caf0e2337246f9eb
yukihane <yukihane.feather@gmail.com>
2022-01-31 16:25:32 +0900

    作りかけ

--- hibernate/hibernate-criteria-case/src/main/java/com/github/yukihane/examples/App.java
+++ hibernate/hibernate-criteria-case/src/main/java/com/github/yukihane/examples/App.java
@@ -1,11 +1,26 @@
 package com.github.yukihane.examples;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  * Hello world!
  */
 public class App {
 
-    public static void main(final String[] args) {
-        System.out.println("Hello World!");
+    public static void main(final String[] args) throws SQLException {
+        SessionFactory sf = new Configuration().configure().buildSessionFactory();
+        Session sess = sf.openSession();
+        Transaction tx = sess.beginTransaction();
+        sess.createSQLQuery("create table my_table(id integer, name varchar(255))").executeUpdate();
+        sess.createSQLQuery("insert into my_table(id,name) values(1,'yamada'),(2,'tanaka'),(3,'suzuki'),(4,'inoue')").executeUpdate();
+        tx.commit();
+        sf.close();
     }
 }
