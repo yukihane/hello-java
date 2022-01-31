cdaa423ac918b6a612cd1c5641aa12535e4fe70e
yukihane <yukihane.feather@gmail.com>
2022-01-31 14:17:59 +0900

    init

---
+++ hibernate/hibernate-criteria-case/src/main/java/com/github/yukihane/examples/App.java
@@ -0,0 +1,11 @@
+package com.github.yukihane.examples;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    public static void main(final String[] args) {
+        System.out.println("Hello World!");
+    }
+}
