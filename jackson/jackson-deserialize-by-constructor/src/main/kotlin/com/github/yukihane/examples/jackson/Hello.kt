98c68baeee00d83c587ad18421d3e06ac4859f67
yukihane <yukihane.feather@gmail.com>
2022-03-04 18:06:38 +0900

    auto-generated files

---
+++ jackson/jackson-deserialize-by-constructor/src/main/kotlin/com/github/yukihane/examples/jackson/Hello.kt
@@ -0,0 +1,6 @@
+package com.github.yukihane.examples.jackson
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
---
+++ jackson/jackson-deserialize-by-constructor/src/test/kotlin/com/github/yukihane/examples/jackson/HelloTest.kt
@@ -0,0 +1,8 @@
+package com.github.yukihane.examples.jackson
+
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class HelloTest {
+
+}
