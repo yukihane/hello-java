082d2a44a88d5c8ed1c4c78fa925010f9c1dbbac
yukihane <yukihane.feather@gmail.com>
2022-03-04 19:34:22 +0900

    kotlinコード

---
+++ jackson/jackson-deserialize-by-constructor/src/main/kotlin/examples/kotlin/KotlinMain.kt
@@ -0,0 +1,16 @@
+package examples.kotlin
+
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.fasterxml.jackson.module.kotlin.kotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import examples.java.JavaMain.JSON_TEXT
+
+fun main() {
+    val om = jacksonObjectMapper()
+    om.registerModule(kotlinModule())
+
+    val person = om.readValue<Person>(JSON_TEXT)
+    println(person)
+}
+
+data class Person(val name: String, val age: Integer)
