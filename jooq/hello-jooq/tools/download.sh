26adaea023bbc9477ed68dfc6ffeaadb39198fbf
yukihane <yukihane.feather@gmail.com>
2022-08-26 02:38:02 +0900

    h2を使わなくなったので不要

--- jooq/hello-jooq/tools/download.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# https://www.h2database.com/html/download.html
-curl -L -o h2.jar 'https://search.maven.org/remotecontent?filepath=com/h2database/h2/2.1.214/h2-2.1.214.jar'
--- jooq/hello-jooq/tools/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `author` (
-  `id` int NOT NULL,
-  `first_name` varchar(255) DEFAULT NULL,
-  `last_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-);
