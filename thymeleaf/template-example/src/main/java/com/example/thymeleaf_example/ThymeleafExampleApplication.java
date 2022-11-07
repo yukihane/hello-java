4e693128b14c519e50d43d74c9c7d9ea02453030
yukihane <yukihane.feather@gmail.com>
2022-11-07 10:29:21 +0900

    init

---
+++ thymeleaf/template-example/src/main/java/com/example/thymeleaf_example/ThymeleafExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafExampleApplication.class, args);
+	}
+
+}
---
+++ thymeleaf/template-example/src/test/java/com/example/thymeleaf_example/ThymeleafExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thymeleaf_example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
