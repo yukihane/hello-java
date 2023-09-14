2e2e0ed367b717ade192a093803f1f5d8fa6dbe1
yukihane <yukihane.feather@gmail.com>
2023-09-15 03:21:40 +0900

    init

---
+++ spring/hello-graphql/src/test/java/com/example/hellographql/HelloGraphqlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hellographql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloGraphqlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
