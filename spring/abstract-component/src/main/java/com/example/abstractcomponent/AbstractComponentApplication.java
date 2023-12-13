1d5ffd6e653aa93a995942364d7ea87a2d60ac1e
yukihane <yukihane.feather@gmail.com>
2023-12-13 20:25:04 +0900

    init

---
+++ spring/abstract-component/src/main/java/com/example/abstractcomponent/AbstractComponentApplication.java
@@ -0,0 +1,13 @@
+package com.example.abstractcomponent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbstractComponentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbstractComponentApplication.class, args);
+	}
+
+}
---
+++ spring/abstract-component/src/test/java/com/example/abstractcomponent/AbstractComponentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.abstractcomponent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbstractComponentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
