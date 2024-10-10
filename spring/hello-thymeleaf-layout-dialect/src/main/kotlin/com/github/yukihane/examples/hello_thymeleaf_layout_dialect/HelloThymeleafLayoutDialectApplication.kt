052b283642e767abd6e0f8dfb006f2c31ff17da3
yukihane <yukihane.feather@gmail.com>
2024-10-10 16:09:18 +0900

    init

---
+++ spring/hello-thymeleaf-layout-dialect/src/main/kotlin/com/github/yukihane/examples/hello_thymeleaf_layout_dialect/HelloThymeleafLayoutDialectApplication.kt
@@ -0,0 +1,11 @@
+package com.github.yukihane.examples.hello_thymeleaf_layout_dialect
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HelloThymeleafLayoutDialectApplication
+
+fun main(args: Array<String>) {
+	runApplication<HelloThymeleafLayoutDialectApplication>(*args)
+}
---
+++ spring/hello-thymeleaf-layout-dialect/src/test/kotlin/com/github/yukihane/examples/hello_thymeleaf_layout_dialect/HelloThymeleafLayoutDialectApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.github.yukihane.examples.hello_thymeleaf_layout_dialect
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HelloThymeleafLayoutDialectApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
