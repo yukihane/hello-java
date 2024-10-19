f4033ce4a98c69a8215b613611ae45c35a71e8a2
yukihane <yukihane.feather@gmail.com>
2024-10-20 04:15:52 +0900

    init

---
+++ spring/spring-boot-auto-restart/maven-kotlin-with-devtools/src/main/kotlin/com/example/maven_kotlin_with_devtools/MavenKotlinWithDevtoolsApplication.kt
@@ -0,0 +1,11 @@
+package com.example.maven_kotlin_with_devtools
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MavenKotlinWithDevtoolsApplication
+
+fun main(args: Array<String>) {
+	runApplication<MavenKotlinWithDevtoolsApplication>(*args)
+}
---
+++ spring/spring-boot-auto-restart/maven-kotlin-with-devtools/src/test/kotlin/com/example/maven_kotlin_with_devtools/MavenKotlinWithDevtoolsApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.maven_kotlin_with_devtools
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MavenKotlinWithDevtoolsApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
