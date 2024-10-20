f26e2c580a3c5fda34575a6231752e66543ab93e
yukihane <yukihane.feather@gmail.com>
2024-10-20 10:27:18 +0900

    hot swappingができるか確認するための編集

--- spring/spring-boot-auto-restart/gradle-kotlin-with-devtools/src/main/kotlin/com/example/gradle_kotlin_with_devtools/MyController.kt
+++ spring/spring-boot-auto-restart/gradle-kotlin-with-devtools/src/main/kotlin/com/example/gradle_kotlin_with_devtools/MyController.kt
@@ -1,13 +1,15 @@
 package com.example.gradle_kotlin_with_devtools
 
 import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.RequestMapping
 
 @Controller
 class MyController {
 
     @RequestMapping
-    fun index(): String {
+    fun index(model: Model): String {
+        model.addAttribute("name", "Gradle v9")
         return "index"
     }
 }
