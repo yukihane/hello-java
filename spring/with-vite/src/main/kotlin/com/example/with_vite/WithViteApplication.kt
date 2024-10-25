f4f9c750d44b10c88f665686a300965354a899b1
yukihane <yukihane.feather@gmail.com>
2024-10-26 01:32:48 +0900

    init

---
+++ spring/with-vite/src/main/kotlin/com/example/with_vite/WithViteApplication.kt
@@ -0,0 +1,11 @@
+package com.example.with_vite
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WithViteApplication
+
+fun main(args: Array<String>) {
+	runApplication<WithViteApplication>(*args)
+}
---
+++ spring/with-vite/src/test/kotlin/com/example/with_vite/WithViteApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.with_vite
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WithViteApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
