9b3ce9ca9e3a125cbe3d7420991d32de64c29bdd
yukihane <yukihane.feather@gmail.com>
2024-11-05 03:01:07 +0900

    init

---
+++ jpa/spring-data-jpa-pagenation/src/main/kotlin/com/example/spring_data_jpa_pagenation/SpringDataJpaPagenationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_data_jpa_pagenation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringDataJpaPagenationApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringDataJpaPagenationApplication>(*args)
+}
---
+++ jpa/spring-data-jpa-pagenation/src/test/kotlin/com/example/spring_data_jpa_pagenation/SpringDataJpaPagenationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.spring_data_jpa_pagenation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringDataJpaPagenationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
