bdf2d04c029207ba440e174660d5337ec62bf3e2
yukihane <yukihane.feather@gmail.com>
2025-02-12 11:35:04 +0900

    init

---
+++ spring/jackson-json-date-type-convert/src/main/kotlin/com/example/jackson_json_date_type_convert/JacksonJsonDateTypeConvertApplication.kt
@@ -0,0 +1,11 @@
+package com.example.jackson_json_date_type_convert
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JacksonJsonDateTypeConvertApplication
+
+fun main(args: Array<String>) {
+	runApplication<JacksonJsonDateTypeConvertApplication>(*args)
+}
---
+++ spring/jackson-json-date-type-convert/src/test/kotlin/com/example/jackson_json_date_type_convert/JacksonJsonDateTypeConvertApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.jackson_json_date_type_convert
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class JacksonJsonDateTypeConvertApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
