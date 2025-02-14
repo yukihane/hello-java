0ab8890439983ce1844b92119a95feeb1d7050c9
yukihane <yukihane.feather@gmail.com>
2025-02-14 20:12:31 +0900

    init

---
+++ spring/clock-bean/src/main/kotlin/com/example/clock_bean/ClockBeanApplication.kt
@@ -0,0 +1,11 @@
+package com.example.clock_bean
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ClockBeanApplication
+
+fun main(args: Array<String>) {
+	runApplication<ClockBeanApplication>(*args)
+}
---
+++ spring/clock-bean/src/test/kotlin/com/example/clock_bean/ClockBeanApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.clock_bean
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ClockBeanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
