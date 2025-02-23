ba2c2791f73063fbea83b3c7f512f3e1c433e700
yukihane <yukihane.feather@gmail.com>
2025-02-24 01:54:41 +0900

    init

---
+++ spring/hexagonal-architecture/src/main/kotlin/com/example/hexagonal_architecture/HexagonalArchitectureApplication.kt
@@ -0,0 +1,11 @@
+package com.example.hexagonal_architecture
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HexagonalArchitectureApplication
+
+fun main(args: Array<String>) {
+	runApplication<HexagonalArchitectureApplication>(*args)
+}
---
+++ spring/hexagonal-architecture/src/test/kotlin/com/example/hexagonal_architecture/HexagonalArchitectureApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.hexagonal_architecture
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class HexagonalArchitectureApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
