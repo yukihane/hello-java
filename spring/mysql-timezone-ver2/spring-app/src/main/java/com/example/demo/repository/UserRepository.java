af29e43e09799f920d80bef07a99bade4cf2d6ba
yukihane <yukihane.feather@gmail.com>
2025-03-19 04:23:48 +0900

    https://start.spring.io/ で生成したものに差し替え

--- spring/mysql-timezone-ver2/spring-app/src/main/java/com/example/demo/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, Long> {
-}
