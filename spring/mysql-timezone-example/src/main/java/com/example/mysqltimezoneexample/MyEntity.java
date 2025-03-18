4fd3cd9029ac2acc995978f87d90e483d15e55bc
yukihane <yukihane.feather@gmail.com>
2025-03-18 19:38:01 +0900

    Spring Bootのバージョンを上たのでjavaxからjakartaにパッケージが変わっている

--- spring/mysql-timezone-example/src/main/java/com/example/mysqltimezoneexample/MyEntity.java
+++ spring/mysql-timezone-example/src/main/java/com/example/mysqltimezoneexample/MyEntity.java
@@ -1,19 +1,18 @@
 package com.example.mysqltimezoneexample;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import lombok.Data;
+import java.time.LocalTime;
 
 @Entity
 @Table(name = "my_entity")
-@Data
+@Getter
+@Setter
 public class MyEntity {
 
     @Id
