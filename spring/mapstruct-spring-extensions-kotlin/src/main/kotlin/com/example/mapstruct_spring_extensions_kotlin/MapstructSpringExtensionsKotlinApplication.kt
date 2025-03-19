1e7b2f5949bc519109dc4e49e23e87c63e9c025e
yukihane <yukihane.feather@gmail.com>
2025-03-20 02:06:00 +0900

    init

---
+++ spring/mapstruct-spring-extensions-kotlin/src/main/kotlin/com/example/mapstruct_spring_extensions_kotlin/MapstructSpringExtensionsKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.mapstruct_spring_extensions_kotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MapstructSpringExtensionsKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<MapstructSpringExtensionsKotlinApplication>(*args)
+}
---
+++ spring/mapstruct-spring-extensions-kotlin/src/test/kotlin/com/example/mapstruct_spring_extensions_kotlin/MapstructSpringExtensionsKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.mapstruct_spring_extensions_kotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MapstructSpringExtensionsKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
