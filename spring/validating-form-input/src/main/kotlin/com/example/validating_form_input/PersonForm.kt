116abcf1a62ac625bdd5dd43bab4bc6901b89ca0
yukihane <yukihane.feather@gmail.com>
2025-03-30 04:48:48 +0900

    アノテーション付け間違え

--- spring/validating-form-input/src/main/kotlin/com/example/validating_form_input/PersonForm.kt
+++ spring/validating-form-input/src/main/kotlin/com/example/validating_form_input/PersonForm.kt
@@ -1,5 +1,6 @@
 package com.example.validating_form_input
 
+import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
@@ -9,6 +10,6 @@ data class PersonForm(
     val name: String?,
 
     @field:NotNull
-    @field:Size(min = 2, max = 30)
+    @field:Min(18)
     val age: Int?,
 )
