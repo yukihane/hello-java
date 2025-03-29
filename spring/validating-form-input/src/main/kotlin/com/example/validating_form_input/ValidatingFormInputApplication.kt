50bcb4e78915bd4b7f30b75e4cca0009bca9749e
yukihane <yukihane.feather@gmail.com>
2025-03-30 03:36:37 +0900

    init

---
+++ spring/validating-form-input/src/main/kotlin/com/example/validating_form_input/ValidatingFormInputApplication.kt
@@ -0,0 +1,11 @@
+package com.example.validating_form_input
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ValidatingFormInputApplication
+
+fun main(args: Array<String>) {
+	runApplication<ValidatingFormInputApplication>(*args)
+}
---
+++ spring/validating-form-input/src/test/kotlin/com/example/validating_form_input/ValidatingFormInputApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.validating_form_input
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ValidatingFormInputApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
