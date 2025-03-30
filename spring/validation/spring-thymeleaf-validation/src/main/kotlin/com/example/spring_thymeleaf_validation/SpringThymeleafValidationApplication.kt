5fd9a835b496296adcbd45338036f6941b9d57d6
yukihane <yukihane.feather@gmail.com>
2025-03-30 12:33:47 +0900

    init

---
+++ spring/validation/spring-thymeleaf-validation/src/main/kotlin/com/example/spring_thymeleaf_validation/SpringThymeleafValidationApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_thymeleaf_validation
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringThymeleafValidationApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringThymeleafValidationApplication>(*args)
+}
---
+++ spring/validation/spring-thymeleaf-validation/src/test/kotlin/com/example/spring_thymeleaf_validation/SpringThymeleafValidationApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.spring_thymeleaf_validation
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringThymeleafValidationApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
