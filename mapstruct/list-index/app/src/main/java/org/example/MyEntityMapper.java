d5c3e5c09d2ad05922ad1f892bdc79c1ed2eb97d
yukihane <yukihane.feather@gmail.com>
2025-04-02 06:55:19 +0900

    qualifiedByName の利用

--- mapstruct/list-index/app/src/main/java/org/example/MyEntityMapper.java
+++ mapstruct/list-index/app/src/main/java/org/example/MyEntityMapper.java
@@ -1,11 +1,32 @@
 package org.example;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Named;
 import org.mapstruct.factory.Mappers;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Mapper
 public interface MyEntityMapper {
     MyEntityMapper INSTANCE = Mappers.getMapper(MyEntityMapper.class);
 
+    @Mapping(target = "children", source = "children", qualifiedByName = "addIndex")
     MyEntity toEntity(MyEntityDto dto);
+
+    @Named("addIndex")
+    default Set<ChildEntity> addIndex(List<ChildEntityDto> dtos) {
+        Set<ChildEntity> entities = new HashSet<>();
+        for (int i = 0; i < dtos.size(); i++) {
+            ChildEntityDto dto = dtos.get(i);
+            ChildEntity entity = new ChildEntity();
+            entity.setName(dto.getName());
+            entity.setIndex(i); // Add index to the child entity
+            entities.add(entity);
+        }
+        return entities;
+    }
+
 }
