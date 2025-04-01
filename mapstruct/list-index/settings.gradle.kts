e7efd0912eea2e201a77191b7d83e1222dd66f15
yukihane <yukihane.feather@gmail.com>
2025-04-02 06:36:38 +0900

    javaでやり直し

--- mapstruct/list-index/settings.gradle.kts
+++ mapstruct/list-index/settings.gradle.kts
@@ -3,7 +3,6 @@
  *
  * The settings file is used to specify which projects to include in your build.
  * For more detailed information on multi-project builds, please refer to https://docs.gradle.org/8.12.1/userguide/multi_project_builds.html in the Gradle documentation.
- * This project uses @Incubating APIs which are subject to change.
  */
 
 plugins {
