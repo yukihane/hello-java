cf9d50e999a9ce0c5f752ec6d85487212cfeadbb
yukihane <yukihane.feather@gmail.com>
2025-04-04 01:11:38 +0900

    同時に複数のエラーを起こせるように

--- spring/validation/table-validation/src/main/kotlin/com/example/table_validation/model/PersonalInfo.kt
+++ spring/validation/table-validation/src/main/kotlin/com/example/table_validation/model/PersonalInfo.kt
@@ -4,14 +4,15 @@ import jakarta.validation.constraints.*
 
 data class PersonalInfo(
     @field:NotBlank(message = "名前は必須です")
-    @field:Size(max = 50, message = "名前は50文字以内で入力してください")
+    @field:Pattern(regexp = "\\d{1,10}", message = "名前は1〜10文字の数字で入力してください")
+    @field:Size(max = 5, message = "名前は50文字以内で入力してください")
     var name: String = "",
-    
+
     @field:NotNull(message = "年齢は必須です")
     @field:Min(value = 0, message = "年齢は0以上で入力してください")
     @field:Max(value = 120, message = "年齢は120以下で入力してください")
     var age: Int? = null,
-    
+
     @field:NotBlank(message = "住所は必須です")
     @field:Size(max = 100, message = "住所は100文字以内で入力してください")
     var address: String = ""
