a928f5d8e043adfc6684dccf224594a5639be658
yukihane <yukihane.feather@gmail.com>
2025-05-01 21:17:55 +0900

    一旦シンプルなformで確認する

--- spring/validation/kotlin-form-binding/src/main/java/com/example/kotlin_form_binding/JavaForm.java
+++ spring/validation/kotlin-form-binding/src/main/java/com/example/kotlin_form_binding/JavaForm.java
@@ -1,6 +1,5 @@
 package com.example.kotlin_form_binding;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -8,8 +7,6 @@ import lombok.Data;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 
 import static org.springframework.format.annotation.DateTimeFormat.ISO.DATE;
 
@@ -34,19 +31,4 @@ public class JavaForm {
      * 年齢(任意)
      */
     private Short age;
-
-    /**
-     * 家族
-     */
-    @Valid
-    @NotNull
-    private List<JavaFamily> families = new ArrayList<>();
-
-    @Data
-    public static class JavaFamily {
-        @NotNull
-        @NotEmpty
-        @Size(min = 1, max = 10)
-        private String familyName;
-    }
 }
